eecc659ba8dec19cb3512ef0f81e791d777a00be
Daniel Llewellyn <daniel@bowlhat.net>
2005-11-05 17:31:10 +0000

    cleaned up the code a bit (replaced tabs with 4space chars, so that it looks the same in everyone's editor. Also updated the printdebug() function to output valid xhtml (hopefully)

--- cgi-bin/index.pl
+++ cgi-bin/index.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl
-
-
-# Crimp
-# Author: Martin Guppy <deadpan110@gmail.com>
+# CRIMP - Content Redirection Internet Management Program
+# Authors: The CRIMP team
+# Project Leads: Martin "Deadpan110" Guppy <deadpan110@sourceforge.net>,
+#                Daniel "Fremen" Llewellyn <diddledan@sourceforge.net>
 # Version: 1.0
-# Home/Docs/Licensing: http://ind-network.co.uk/apps/crimp/
+# Home/Docs/Licensing: http://crimp.sourceforge.net/
 
 package Crimp;
 #
@@ -29,9 +29,6 @@ use CGI;
 use CGI::Carp qw/ fatalsToBrowser /;
 #use Fcntl; 
 
-
-
-
 my $query = new CGI;
 #print $query->header('text/html','200');
 
@@ -41,27 +38,18 @@ my $query = new CGI;
 #DocumentTemplate;ContentDirectory;ContentType
 our $crimp;
 $crimp = {
+    IniCommands => "FrameRedirect;VirtualRedirect;ContentType;ContentDirectory;DocumentTemplate",
+    RemoteHost => "$ENV{'REMOTE_ADDR'}",
+    ServerName =>  "$ENV{'SERVER_NAME'}",
+    ServerSoftware =>  "$ENV{'SERVER_SOFTWARE'}",
+    UserAgent =>  "$ENV{'HTTP_USER_AGENT'}",
+    HttpRequest =>  "$ENV{'REDIRECT_URL'}",
+    HttpQuery =>  "$ENV{'REDIRECT_QUERY_STRING'}",
+    ExitCode => "500",
+    DebugMode => "off",
+};
 
-IniCommands => "FrameRedirect;VirtualRedirect;ContentType;ContentDirectory;DocumentTemplate",
-				
-				RemoteHost => "$ENV{'REMOTE_ADDR'}",
-				ServerName =>  "$ENV{'SERVER_NAME'}",
-				ServerSoftware =>  "$ENV{'SERVER_SOFTWARE'}",
-				UserAgent =>  "$ENV{'HTTP_USER_AGENT'}",
-				HttpRequest =>  "$ENV{'REDIRECT_URL'}",
-				HttpQuery =>  "$ENV{'REDIRECT_QUERY_STRING'}",
- 				ExitCode => "500",
-				DebugMode => "off",
-				
-		
-				
- 
-    };
-
-if ($crimp->{HttpQuery}){$crimp->{HttpQuery} = "?$crimp->{HttpQuery}";}
-
-
-
+if ($crimp->{HttpQuery}) { $crimp->{HttpQuery} = join '', '?', $crimp->{HttpQuery}; }
 
 
 ####################################################################
@@ -83,51 +71,40 @@ if ($i < 8){$tune = "$tune -n ";}
 #$BEEP = `beep`;
 ####################################################################
 
-&printdebug("CRIMP [content redirection internet management program] (Debug View)");
-&printdebug("Details","","Author: Martin Guppy [deadpan110\@gmail.com]","Version: 1.0","Home/Docs/Licensing: http://ind-network.co.uk/apps/crimp/");
+&printdebug("CRIMP [Content Redirection Internet Management Program] (Debug View)");
+&printdebug("Details", "", "Authors: The CRIMP Team", "Version: 1.0", "http://crimp.sourceforge.net/");
 
 &printdebug(
-	"System Variables",
-	"pass",
-	
-	"ServerName: $crimp->{ServerName}",
-	"ServerSoftware: $crimp->{ServerSoftware}"
-	);
+    "System Variables",
+    "pass",
+    "ServerName: $crimp->{ServerName}",
+    "ServerSoftware: $crimp->{ServerSoftware}"
+);
 
 &printdebug(
-	"User Variables",
-	"pass",
-	
-	"RemoteHost: $crimp->{RemoteHost}",
-	"UserAgent: $crimp->{UserAgent}",
-	"HttpRequest: $crimp->{HttpRequest}"
-	);
-
-
+    "User Variables",
+    "pass",
+    "RemoteHost: $crimp->{RemoteHost}",
+    "UserAgent: $crimp->{UserAgent}",
+    "HttpRequest: $crimp->{HttpRequest}"
+);
 
 if ((!-e "crimp.ini")||(!-e "Config/Tiny.pm")){
-$crimp = {DebugMode => 'on',};
-printdebug(
-	"Crimp Files not found",
-	"warn",
-
-	"Please check the following files exist in the crimp directory"
-	);
+    $crimp = {DebugMode => 'on',};
+    printdebug(
+        "Crimp Files not found",
+        "warn",
+        "Please check the following files exist in the crimp directory"
+    );
 }
-
 
 our $Config = Config::Tiny->new();
 $Config = Config::Tiny->read( 'crimp.ini' );
 
-
-
 #switch to debug mode if set in crimp.ini
 if ($crimp->{DebugMode} ne "on"){
-
-#$crimp={DebugMode => "$Config->{_}->{DebugMode}"};
-$crimp->{DebugMode}=$Config->{_}->{DebugMode};
+    $crimp->{DebugMode}=$Config->{_}->{DebugMode};
 }
-
 
 #set default values
 #$crimp->{DocumentTemplate}=$Config->{_}->{DocumentTemplate};
@@ -147,40 +124,27 @@ $crimp->{DebugMode}=$Config->{_}->{DebugMode};
 #   /home/news
 #	/home
 
-
 @HttpRequest = split(/\//,$crimp->{HttpRequest});
 
 foreach $HttpRequest (@HttpRequest){
+    if ($HttpRequest ne "") {
+        $tempstr = "$tempstr/$HttpRequest";
+    }
 
-
-
-
-if ($HttpRequest ne "") {
-$tempstr = "$tempstr/$HttpRequest";
+    if ($Config->{$tempstr}){
+        $crimp->{UserConfig}=$tempstr;
+    }
 }
-
-
-
-if ($Config->{$tempstr}){
-$crimp->{UserConfig}=$tempstr;
-}
-
-}
-
 
 if ($crimp->{UserConfig} eq ""){
-$crimp->{UserConfig}="/";
+    $crimp->{UserConfig}="/";
 }
 
 &printdebug(
-	"'crimp.ini' Variables",
-	"pass",
-
-	"UserConfig: $crimp->{UserConfig}",
-
-
-
-	);
+    "'crimp.ini' Variables",
+    "pass",
+    "UserConfig: $crimp->{UserConfig}",
+);
 
 ####################################################################
 #set variables
@@ -188,103 +152,80 @@ $crimp->{UserConfig}="/";
 @IniCommands = split(/\;/,$crimp->{IniCommands});
 
 foreach $IniCommands (@IniCommands){
+    if (!$crimp->{$IniCommands}) {
+        if ($Config->{$crimp->{UserConfig}}->{$IniCommands}) {
+            $crimp->{$IniCommands}=$Config->{$crimp->{UserConfig}}->{$IniCommands};
+        } else {
+            if ($Config->{_}->{$IniCommands}) {
+                $crimp->{$IniCommands}=$Config->{_}->{$IniCommands};
+            }
+        }
+    }
+    #print "$IniCommands $crimp->{$IniCommands}<br>";
 
-if (!$crimp->{$IniCommands}){
-
-	if ($Config->{$crimp->{UserConfig}}->{$IniCommands}){
-	$crimp->{$IniCommands}=$Config->{$crimp->{UserConfig}}->{$IniCommands};
-	}
-	else{
-		if ($Config->{_}->{$IniCommands}){
-			$crimp->{$IniCommands}=$Config->{_}->{$IniCommands};
-			}
-	}
+    #Load Module
+    if ($crimp->{$IniCommands} ne ""){
+        if ( !-e "Crimp/$IniCommands.pm"){
+            printdebug("Module '$IniCommands' not found","warn","Check 'crimp.ini' for the following:","$IniCommands = $crimp->{$IniCommands}");
+        }else{
+            #	printdebug("Module '$IniCommands' loading","pass","click here to get this file");
+            require "Crimp/$IniCommands.pm";
+        }
+    }	
 }
-#print "$IniCommands $crimp->{$IniCommands}<br>";
 
-#Load Module
-if ($crimp->{$IniCommands} ne ""){
-
-		if ( !-e "Crimp/$IniCommands.pm"){
-			printdebug("Module '$IniCommands' not found","warn","Check 'crimp.ini' for the following:","$IniCommands = $crimp->{$IniCommands}");
-			}
-		else{
-		#	printdebug("Module '$IniCommands' loading","pass","click here to get this file");
-			require "Crimp/$IniCommands.pm";
-			}
-}	
-
-
-}
 ####################################################################
 ## The End ##
 ############
 
 if (($crimp->{ExitCode} ne "200")&&($crimp->{DisplayHtml} ne "")){
-$crimp->{ExitCode} = "200";
+    $crimp->{ExitCode} = "200";
 }
 
-
 #This is where we finish the document or file
-
 print $query->header('text/html',$crimp->{ExitCode});
-
 printdebug("Crimp Exit","pass","Error code: $crimp->{ExitCode}");
-			
 print "$crimp->{DisplayHtml}";
 
-
-
-
 ####################################################################
+
 if ($crimp->{DebugMode} eq "on"){
-print "<br>$PRINT_DEBUG";
+    print "<br />$PRINT_DEBUG";
 }
 
 #foreach $item (keys %ENV) { print "$item = $ENV{$item}\n<br>";}
 
-
 ####################################################################
-
-
-
-
 ####################################################################
 
 
 sub printdebug(){
+    my $solut="";
+    my $mssge=shift(@_);
+    my $stats=shift(@_);
+    my $extra=shift(@_);
 
-my $solut="";
-my $mssge=shift(@_);
-my $stats=shift(@_);
-my $extra=shift(@_);
+    #print "$Config->{_}->{Debug};";
+    while ($extra){
+        $solut="$solut<br />&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: #ccc;'>$extra</span>";
+        $extra=shift(@_);
+    }
 
+    if ($stats eq "pass"){$stats="<pre><span style='color: #fff;'>[<span style='color: #0f0;'>PASS</span>]</span></pre>"}
+    if ($stats eq "warn"){$stats="<pre><span style='color: #fff;'>[<span style='color: #fc3;'>WARN</span>]</span></pre>"}
+    if ($stats eq "exit"){$stats="<pre><span style='color: #fff;'>[<span style='color: #33f;'>EXIT</span>]</span></pre>";$quits=1;}
+    if ($stats eq "fail"){$stats="<pre><span style='color: #fff;'>[<span style='color: #f00;'>FAIL</span>]</span></pre>";$quits=1;}
 
+    if ($solut ne ""){$mssge="<b>&#149;</b> $mssge $solut"}
 
-#print "$Config->{_}->{Debug};";
-while ($extra){
-$solut="$solut<br>&nbsp;&nbsp;&nbsp;&nbsp;<font color='#CCCCCC'>$extra</font>";
-$extra=shift(@_);
+    $PRINT_DEBUG = "$PRINT_DEBUG<table style='width: 100%; border:0; border-collapse: collapse; background-color: #000;'><tr><td style='text-align: left; vertcal-align: top; color: #fff;'><pre>$mssge</pre></td><td style='text-align :right; vertical-align: top;'>$stats</td></tr></table>";
+    #
+    #<table width='100%' border='0' cellspacing='0' cellpadding='0'><tr bgcolor='#CCCCCC'><td align='left' valign='top'><h6><font face='Verdana, Arial, Helvetica, sans-serif' size='1'>Powered by Crimp &copy;2004 IND-Web.com</font></h6><td align='right' valign='top'><font face='Verdana, Arial, Helvetica, sans-serif' size='1'><b>admin</b></font></td></tr><tr bgcolor='#000000'><td colspan='2'>$PRINT_DEBUG</td></tr></table>
+    if ($quits){
+        crimp_display("debug");
+        #print "META <meta http-equiv='refresh' content='30;URL=../cgi-bin/crimp.pl?mode=config'>";
+        #print "<table width='100%' border='0' cellspacing='0' cellpadding='0'><tr bgcolor='#CCCCCC'><td align='left' valign='top'><h6><font face='Verdana, Arial, Helvetica, sans-serif' size='1'>Powered by Crimp &copy;2004 IND-Web.com</font></h6><td align='right' valign='top'><font face='Verdana, Arial, Helvetica, sans-serif' size='1'><b>admin</b></font></td></tr><tr bgcolor='#000000'><td colspan='2'>$PRINT_DEBUG</td></tr></table>";
+    }
 }
 
-if ($stats eq "pass"){$stats="<pre><font color='#FFFFFF'>[<font color='#00FF00'>PASS</font>]</font></pre>"}
-if ($stats eq "warn"){$stats="<pre><font color='#FFFFFF'>[<font color='#FFCC33'>WARN</font>]</font></pre>"}
-if ($stats eq "exit"){$stats="<pre><font color='#FFFFFF'>[<font color='#3333FF'>EXIT</font>]</font></pre>";$quits=1;}
-if ($stats eq "fail"){
-$quits=1;
-$stats="<pre><font color='#FFFFFF'>[<font color='#FF0000'>FAIL</font>]</font></pre>"}
-
-if ($solut ne ""){$mssge="<b>&#149;</b> $mssge $solut"}
-
-$PRINT_DEBUG = "$PRINT_DEBUG<table width='100%' border='0' cellspacing='0' cellpadding='0' bgcolor='#000000' align='center'><tr><td align='left' valign='top'><font color='#FFFFFF'><pre>$mssge</pre></font></td><td align='right' valign='top'>$stats</td></tr></table>";
-#
-#<table width='100%' border='0' cellspacing='0' cellpadding='0'><tr bgcolor='#CCCCCC'><td align='left' valign='top'><h6><font face='Verdana, Arial, Helvetica, sans-serif' size='1'>Powered by Crimp &copy;2004 IND-Web.com</font></h6><td align='right' valign='top'><font face='Verdana, Arial, Helvetica, sans-serif' size='1'><b>admin</b></font></td></tr><tr bgcolor='#000000'><td colspan='2'>$PRINT_DEBUG</td></tr></table>
-if ($quits){
-crimp_display("debug");
-#print "META <meta http-equiv='refresh' content='30;URL=../cgi-bin/crimp.pl?mode=config'>";
-#print "<table width='100%' border='0' cellspacing='0' cellpadding='0'><tr bgcolor='#CCCCCC'><td align='left' valign='top'><h6><font face='Verdana, Arial, Helvetica, sans-serif' size='1'>Powered by Crimp &copy;2004 IND-Web.com</font></h6><td align='right' valign='top'><font face='Verdana, Arial, Helvetica, sans-serif' size='1'><b>admin</b></font></td></tr><tr bgcolor='#000000'><td colspan='2'>$PRINT_DEBUG</td></tr></table>";
-
-}
-}
-
-
+#REALLY THE END#
