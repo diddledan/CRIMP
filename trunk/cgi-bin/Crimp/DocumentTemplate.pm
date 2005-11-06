0d865bffda451b81576a35d8722cffb16dc7f0a9
diddledan <diddledan@9e2adf73-833e-0410-8d6d-913abc08db82>
2005-11-06 18:02:41 +0000

    tidied up, and changed the logic of inserting the page's content into the template

--- trunk/cgi-bin/Crimp/DocumentTemplate.pm
+++ trunk/cgi-bin/Crimp/DocumentTemplate.pm
@@ -1,58 +1,26 @@
-#language pack
-
-my $new_content="";
-
 @HttpRequest = split(/\//,$crimp->{HttpRequest});
-$a=0;
 
 foreach $HttpRequest (@HttpRequest){
-#print "$crimp->{HttpRequest} :: $HttpRequest :: $crimp->{UserConfig}<br>";
-if ($crimp->{UserConfig} ne "/$HttpRequest"){$path = "$path/$HttpRequest";}
-$a++;
+  #print "$crimp->{HttpRequest} :: $HttpRequest :: $crimp->{UserConfig}<br>";
+  if ($crimp->{UserConfig} ne "/$HttpRequest"){$path = "$path/$HttpRequest";}
 }
 
-
-
-sysopen (FILE,"$crimp->{DocumentTemplate}", O_RDONLY) or exit;
+sysopen (FILE,$crimp->{DocumentTemplate}, O_RDONLY) or exit;
 @template_content=<FILE>;
 #$SIZE=@LINES;
-$status = "pass";
+$status = 'pass';
 close(FILE);
 
+my $new_content = '';
+foreach $template_line(@template_content) {
+  $new_content = join '', $new_content, $template_line;
+}
 
-$a=1;
-foreach $template_content(@template_content) {
-#printdebug ("parsing template $a ");
-$a++;
-
-#chop($template_content) if $line =~ /\n$/;
-
-#
-
-while ($template_content =~ /<!--PAGE_CONTENT-->/gi){
 #printdebug ("Putting Page into Template");
-$template_content =~ s//$crimp->{DisplayHtml}/g;;
-}
-
-$new_content= "$new_content$template_content\n\n";
-    
-}
-
+$new_content =~ s/<!--PAGE_CONTENT-->/$crimp->{DisplayHtml}/gi;
 
 $crimp->{DisplayHtml} = $new_content;
 
-
-
-
-
-
 &printdebug("Module 'DocumentTemplate'","$status","Started With: $crimp->{DocumentTemplate}");
 
-
 1;
-
-
-
-
-
-
