04da4368758c8db77c6bd9ca7662629103a4feb4
Daniel Llewellyn <daniel@bowlhat.net>
2005-11-06 18:42:29 +0000

    titied up a bit

--- cgi-bin/Crimp/VirtualRedirect.pm
+++ cgi-bin/Crimp/VirtualRedirect.pm
@@ -1,74 +1,47 @@
 #Crimp Module Virtual Redirect
-
-
 #our $crimp;
 
-my $path="";
 #parse url and remove the config file section $crimp->{UserConfig}
-if ($crimp->{DisplayHtml} ne "" ){&printdebug("Module 'VirtualRedirect'","warn",
-		"DisplayHtml has already been filled with content"
-	);
+if ($crimp->{DisplayHtml} ne "" ){
+  &printdebug("Module 'VirtualRedirect'","warn", "DisplayHtml has already been filled with content");
 }
 
-
-
 @HttpRequest = split(/\//,$crimp->{HttpRequest});
-$a=0;
 
+my $path = '';
 foreach $HttpRequest (@HttpRequest){
-#print "$crimp->{HttpRequest} :: $HttpRequest :: $crimp->{UserConfig}<br>";
-if ($crimp->{UserConfig} ne "/$HttpRequest"){$path = "$path/$HttpRequest";}
-$a++;
+  #print "$crimp->{HttpRequest} :: $HttpRequest :: $crimp->{UserConfig}<br>";
+  if ($crimp->{UserConfig} ne "/$HttpRequest"){$path = "$path/$HttpRequest";}
 }
 
 #print "path : $path<br>";
-
-
 #print "<BASE href='http://test.co.uk$crimp->{UserConfig}'>";
 
-
-
-
 use LWP::UserAgent;
-	$ua = LWP::UserAgent->new;
+$ua = LWP::UserAgent->new;
 
-	if ($use_proxy){
-&printdebug("Using proxy server");
-#$ua->proxy(http => "http://$use_proxy");
-	}
+if ($use_proxy){
+  &printdebug("Using proxy server");
+  #$ua->proxy(http => "http://$use_proxy");
+}
 
-  $ua->agent("Mozilla/4.0 (crimp user $crimp->{RemoteHost}\@ind-web.com)"); # pretend we are very capable browser
-  $ua->timeout("30");
+$ua->agent("Mozilla/4.0 (crimp user $crimp->{RemoteHost}\@$crimp->{ServerName})"); # pretend we are very capable browser
+$ua->timeout("30");
 
 #$crimp->{HttpRequest}
 $req = HTTP::Request->new(GET => "$crimp->{VirtualRedirect}$path$crimp->{HttpQuery}");
-
-
-	$req->header('Accept' => '*/*');
-  	$res = $ua->request($req);
+$req->header('Accept' => '*/*');
+$res = $ua->request($req);
 
 if ($res->is_success) {
-#printdebug("File exists on remote server");
-
-
-
-&printdebug("Module 'VirtualRedirect'","pass","Started With: $crimp->{VirtualRedirect}","Fetching the following content:","$crimp->{VirtualRedirect}$path$crimp->{HttpQuery}","REM: This url needs fixing by removing config entry from url");
-$crimp->{DisplayHtml}=$res->content;
-#print $res->content;
-
+  #printdebug("File exists on remote server");
+  &printdebug("Module 'VirtualRedirect'","pass","Started With: $crimp->{VirtualRedirect}","Fetching the following content:","$crimp->{VirtualRedirect}$path$crimp->{HttpQuery}");
+  $crimp->{DisplayHtml}=$res->content;
+  #print $res->content;
+} else {
+  &printdebug("Module 'VirtualRedirect'","warn","Started With: $crimp->{VirtualRedirect}","Fetching the following content:","$crimp->{VirtualRedirect}$path$crimp->{HttpQuery}","connection error");
+  $crimp->{DisplayHtml}="<span style='color: f00;'>Connection error</span>";
 }
-else {
-
-&printdebug("Module 'VirtualRedirect'","warn","Started With: $crimp->{VirtualRedirect}","Fetching the following content:","$crimp->{VirtualRedirect}$path$crimp->{HttpQuery}","connection error");
-$crimp->{DisplayHtml}="Connection error";
-
-};
-
-
-
-
-
 
 #on success
-
 1;
