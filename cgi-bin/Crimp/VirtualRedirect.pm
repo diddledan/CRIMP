e21a112b08fd719b6d42e246e4061acae8708e5d
Daniel Llewellyn <daniel@bowlhat.net>
2005-11-08 17:55:31 +0000

    WOO - LINK REWRITING GOODNESS!!!
    Yups, i've been working hard on the link rewriting - so hard, I've forgotten most of how this new code works.. but it does.. so lets leave it at that, shall we? :-p

--- cgi-bin/Crimp/VirtualRedirect.pm
+++ cgi-bin/Crimp/VirtualRedirect.pm
@@ -14,7 +14,9 @@ foreach $HttpRequest (@HttpRequest){
   if ($crimp->{UserConfig} ne "/$HttpRequest"){$path = join '/', $path, $HttpRequest;}
 }
 #strip off preceeding slashes, as these should be defined in the crimp.ini file
-$path =~ s|^/*||;
+$path =~ s|^/+||;
+#add on any trailing slashes that may be required - another nasty hack by Fremen
+if (($crimp->{HttpRequest} =~ m|/$|) && (!($path =~ m|/$|))) { $path = join '', $path, '/'; }
 
 #print "path : $path<br>";
 #print "<BASE href='http://test.co.uk$crimp->{UserConfig}'>";
@@ -50,17 +52,32 @@ $crimp->{DisplayHtml}= $res->content;
 # Change ServerName to ServerName/Userconfig
 eval {require HTML::TokeParser;};
 if (!$@) {
+  # check that the path doesn't contain any filenames (directories only please)
+  if ($path =~ m|/[\w\-_\.]+$|) {
+    $path = s|/[\w\-_\.]+$|/|;
+  }
   my $token_parser = HTML::TokeParser->new(\$crimp->{DisplayHtml});
   my @image_urls;
+  my @link_urls;
+  my %seenimgs = {};
   while (my $token = $token_parser->get_tag('img')) {
-    my $url = $token->[1]{src} || next;
-    if (!($url =~ m|^http[s]?://|i)) { push @image_urls, $url; }
+    my $url = $token->[1]{'src'} || next;
+    if (!($url =~ m|^http[s]?://|i)) { push(@image_urls, $url) unless ($seenimgs{$url}++); }
   }
+  my %seenlinks = {};
+  #reset token_parser
+  $token_parser = HTML::TokeParser->new(\$crimp->{DisplayHtml});
+  while (my $token = $token_parser->get_tag('a')) {
+    my $url = $token->[1]{'href'} || next;
+    if (!($url =~ m|^.+?:(//)?|i)) { push(@link_urls, $url) unless ($seenlinks{$url}++); }
+  }
+  
   my $i = 0;
   my $url = $crimp->{VirtualRedirect};
   $url =~ m|(^http[s]?://.+?/)|i;
   $url = $1;
   for $image_url (@image_urls) {
+    my $url2 = '';
     if ($image_url =~ m|^/|) { $url2 = $url; }
     else { $url2 = join '', $crimp->{VirtualRedirect}, $path, '/'; }
     my $newimageurl = join '', $url2, $image_url;
@@ -71,16 +88,45 @@ if (!$@) {
     $i++;
   }
   &printdebug('Correcting Images', 'pass', "Using $url for image urls", "Converted $i image tags to point to the correct web location");
+  
+  my $proto = 'http://';
+  if ($ENV{'SERVER_PORT'} eq '443') { $proto = 'https://'; }
+  $url = join '', $proto, $crimp->{ServerName}, '/';
+  my $j = 0;
+  for $link_url (@link_urls) {
+    if ($link_url eq '/') {
+      $crimp->{DisplayHtml} =~ s|(href=['"]{1})/(['"]{1})|\1/\2|g;
+    } else {
+      my $newlinkurl = '';
+      if ($link_url =~ m|^/.+|) {
+        $crimp->{VirtualRedirect} =~ m|^(http[s]?://.*?/)|;
+        if ($crimp->{VirtualRedirect} =~ m/^$1$/) {
+          $newlinkurl = join '', $crimp->{UserConfig}, $link_url;
+        } else {
+          $newlinkurl = join '', $crimp->{VirtualRedirect}, $path, $link_url;
+        }
+      } elsif (!($link_url =~ m|^.+?:(//)?|)) {
+          $newlinkurl = join '', $crimp->{VirtualRedirect}, $path, '/', $link_url;
+      } else { $newlinkurl = $link_url; }
+      $newlinkurl =~ s!^((((f|ht){1}tp[s]?)|(irc)?)://)!!i;
+      my $newlinkproto = $1;
+      $newlinkurl =~ s|/{2,}|/|g;
+      $crimp->{DisplayHtml} =~ s/$link_url/$newlinkproto$newlinkurl/g;
+    }
+    $j++;
+  }
+  &printdebug('Correcting Links', 'pass', "Successfuly converted $j links to point to the right place.");
+  #foreach $item (keys %ENV) { &printdebug("$item = $ENV{$item}"); }
 } else {
-  &printdebug('Correcting Images', 'warn', 'Couldn\'t correct the image urls of this page:', $@, 'Make sure you have installed the HTML::TokeParser module');
+  &printdebug('Correcting Images and Links', 'warn', 'Couldn\'t correct the image and link urls of this page:', $@, 'Make sure you have installed the HTML::TokeParser module');
 }
 
 
-&printdebug("Correct Links","warn","Work in progress","Change all occurences of $crimp->{ServerName} to $crimp->{ServerName}$crimp->{UserConfig}");
+#&printdebug('Correct Links','warn','Work in progress',"Change all occurences of $crimp->{ServerName} to $crimp->{ServerName}$crimp->{UserConfig}");
 #foreach $display_content($crimp->{DisplayHtml}) {
 #$a++;
-$changeto="$crimp->{ServerName}$crimp->{UserConfig}";
-$crimp->{DisplayHtml} =~ s/$crimp->{ServerName}/$changeto/gi;
+#$changeto="$crimp->{ServerName}$crimp->{UserConfig}";
+#$crimp->{DisplayHtml} =~ s/$crimp->{ServerName}/$changeto/gi;
 #$new_content= "$new_content$display_content\n\n";
 #}
 #&printdebug("Correct Links","pass","Change Links and Locations","Changed $count occurences of $crimp->{ServerName} to $crimp->{ServerName}$crimp->{UserConfig}");
