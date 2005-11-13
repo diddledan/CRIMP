5f42c33ecbcba49d827bff00562ee59190f8656e
diddledan <diddledan@9e2adf73-833e-0410-8d6d-913abc08db82>
2005-11-13 19:12:35 +0000

    added facility to have the page title automatically generated by ContentDirectory and inserted into the template wherever the parser sees "<!--TITLE-->"

--- trunk/cgi-bin/Crimp/DocumentTemplate.pm
+++ trunk/cgi-bin/Crimp/DocumentTemplate.pm
@@ -1,26 +1,32 @@
-@HttpRequest = split(/\//,$crimp->{HttpRequest});
-
-foreach $HttpRequest (@HttpRequest){
-  #print "$crimp->{HttpRequest} :: $HttpRequest :: $crimp->{UserConfig}<br>";
-  if ($crimp->{UserConfig} ne "/$HttpRequest"){$path = "$path/$HttpRequest";}
+#only parse the template if this is an html or xhtml page
+if (($crimp->{ContentType} ne 'text/html') || ($crimp->{ContentType} ne 'text/xhtml+xml')) {
+	@HttpRequest = split(/\//,$crimp->{HttpRequest});
+	foreach $HttpRequest (@HttpRequest){
+	  #print "$crimp->{HttpRequest} :: $HttpRequest :: $crimp->{UserConfig}<br>";
+	  if ($crimp->{UserConfig} ne "/$HttpRequest"){$path = "$path/$HttpRequest";}
+	}
+	
+	sysopen (FILE,$crimp->{DocumentTemplate}, O_RDONLY) or exit;
+	@template_content=<FILE>;
+	#$SIZE=@LINES;
+	$status = 'pass';
+	close(FILE);
+	
+	my $new_content = '';
+	foreach $template_line(@template_content) {
+	  $new_content = join '', $new_content, $template_line;
+	}
+	
+	#printdebug ("Putting Page into Template");
+	$new_content =~ s/<!--TITLE-->/$crimp->{PageTitle}/gi;
+	$new_content =~ s/<!--PAGE_CONTENT-->/$crimp->{DisplayHtml}/gi;
+	
+	$crimp->{DisplayHtml} = $new_content;
+	
+	&printdebug("Module 'DocumentTemplate'","$status","Started With: $crimp->{DocumentTemplate}");
+} else {
+	&printdebug('Module \'DocumentTemplate\'', 'pass', "Skipped module for ContentType: $crimp->{ContentType}");
 }
 
-sysopen (FILE,$crimp->{DocumentTemplate}, O_RDONLY) or exit;
-@template_content=<FILE>;
-#$SIZE=@LINES;
-$status = 'pass';
-close(FILE);
-
-my $new_content = '';
-foreach $template_line(@template_content) {
-  $new_content = join '', $new_content, $template_line;
-}
-
-#printdebug ("Putting Page into Template");
-$new_content =~ s/<!--PAGE_CONTENT-->/$crimp->{DisplayHtml}/gi;
-
-$crimp->{DisplayHtml} = $new_content;
-
-&printdebug("Module 'DocumentTemplate'","$status","Started With: $crimp->{DocumentTemplate}");
 
 1;
