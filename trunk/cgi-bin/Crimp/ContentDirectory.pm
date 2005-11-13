d30191f794d541c3118da20110798510b3678398
diddledan <diddledan@9e2adf73-833e-0410-8d6d-913abc08db82>
2005-11-13 19:54:39 +0000

    this shouldn't have been modified, but cvs is telling me that I need to commit my changes, so I am

--- trunk/cgi-bin/Crimp/ContentDirectory.pm
+++ trunk/cgi-bin/Crimp/ContentDirectory.pm
@@ -13,7 +13,7 @@ if ($crimp->{DisplayHtml} ne "" ){
     #check for directory here if it is then use $path
     #make sure the requested file is _NOT_ a directory (Fremen)
     my $requested = join('', $crimp->{ContentDirectory}, $path);
-    if ( !-e $requested ) { $crimp->{ExitCode} = "404"; &printdebug("Module 'ContentDirectory'", 'fail', "$requested does not exist. Please check the URL and try again."); }
+    if ( !-e $requested ) { $crimp->{ExitCode} = '404'; &printdebug("Module 'ContentDirectory'", 'fail', "$requested does not exist. Please check the URL and try again."); }
     if ( -d $requested ) { &printdebug("Module 'ContentDirectory'", 'fail', "$requested is a directory. I cannot open this."); }
     sysopen (FILE,$requested,O_RDONLY) or &printdebug("Module 'ContentDirectory'", 'fail', "Couldn't open file for reading", "file: $requested", "error: $!");
     @display_content=<FILE>;
@@ -25,6 +25,19 @@ if ($crimp->{DisplayHtml} ne "" ){
     foreach $display_content(@display_content) {
         $new_content= "$new_content$display_content";
     }
+    
+	#remove xml header if present
+	$new_content =~ s|<\?xml.*?\?>||i;
+	#remove doctype if present
+	$new_content =~ s|<!DOCTYPE.*?>||i;
+	#remove headers storing the title of the page
+	$new_content =~ s|<title>(.*?)</title>||si;
+	$crimp->{PageTitle} = $1;
+	&printdebug($crimp->{PageTitle});
+	#strip from <html> down to the opening <body> tag
+	$new_content =~ s|<html.*?>.*?<body>||si;
+	#remove the closing </body> tag and any cruft after - alas, that's nothing to do with the dogshow
+	$new_content =~ s|</body>.*||si;
     
     $crimp->{DisplayHtml} = $new_content;
     
