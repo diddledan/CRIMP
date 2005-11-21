0b8a4e3efcd505d09660ba6288a1a567c29e86b3
diddledan <diddledan@9e2adf73-833e-0410-8d6d-913abc08db82>
2005-11-21 16:27:27 +0000

    this appears to work - I've sped things up a bit and hopefully sorted out the exit code

--- trunk/cgi-bin/Crimp/ContentDirectory.pm
+++ trunk/cgi-bin/Crimp/ContentDirectory.pm
@@ -1,4 +1,4 @@
-$ID = q$Id: ContentDirectory.pm,v 1.10 2005-11-18 21:03:06 ind-network Exp $;
+$ID = q$Id: ContentDirectory.pm,v 1.11 2005-11-21 16:27:27 diddledan Exp $;
 &printdebug('Module ContentDirectory',
 			'',
 			'Authors: The CRIMP Team',
@@ -9,62 +9,73 @@ $ID = q$Id: ContentDirectory.pm,v 1.10 2005-11-18 21:03:06 ind-network Exp $;
 @HttpRequest = split(/\//,$crimp->{HttpRequest});
 
 foreach $HttpRequest (@HttpRequest){
-    #printdebug "$crimp->{HttpRequest} :: $HttpRequest :: $crimp->{UserConfig}<br>";
-    if ($crimp->{UserConfig} ne "/$HttpRequest"){$path = "$path/$HttpRequest";}
+	#printdebug "$crimp->{HttpRequest} :: $HttpRequest :: $crimp->{UserConfig}<br>";
+	if ($crimp->{UserConfig} ne "/$HttpRequest"){$path = "$path/$HttpRequest";}
 }
 
 if ($path eq '') { $path = '/index.html'; }
 
 if ($crimp->{DisplayHtml} ne "" ){
-    &printdebug("","warn", "DisplayHtml has already been filled with content");
+	&printdebug("","warn", "DisplayHtml has already been filled with content");
 }else{
-    #check for directory here if it is then use $path
-    #make sure the requested file is _NOT_ a directory (Fremen)
-    my $requested = join('',$crimp->{ContentDirectory}, $path);
-	if ( -d $requested ) {$requested = "$requested/index.html";}
-#my $requested = "$crimp->{HomeDirectory}$crimp->{ContentDirectory}$path";
+	#check for directory here if it is then use $path
+	#make sure the requested file is _NOT_ a directory (Fremen)
+	my $requested = join('',$crimp->{ContentDirectory}, $path);
+	if ( -d $requested ) { $requested = join '/', $requested, 'index.html'; }
+	#my $requested = "$crimp->{HomeDirectory}$crimp->{ContentDirectory}$path";
 
-#&printdebug("$crimp->{HomeDirectory}","","$requested");
+	#&printdebug("$crimp->{HomeDirectory}","","$requested");
 
-   if ( !-e $requested ) {$crimp->{ExitCode} = '404'; }
-   if ( -d $requested ) { $crimp->{ExitCode} = '404'; }
-	sysopen (FILE,"$requested",O_RDONLY) || &printdebug("", 'warn', "Couldn't open file for reading", "file: $requested", "error: $!");
-	
-    @display_content=<FILE>;
-    close(FILE);
-    
-    my $new_content='';
-    
-    ####
-    foreach $display_content(@display_content) {
-        $new_content= "$new_content$display_content";
-    }
-    
-	#remove xml header if present
-	$new_content =~ s|<\?xml.*?\?>||i;
-	#remove doctype if present
-	$new_content =~ s|<!DOCTYPE.*?>||i;
-	#remove headers storing the title of the page
-	$new_content =~ s|<title>(.*?)</title>||si;
-	
-	$crimp->{PageTitle} = $1;
-	if ($crimp->{PageTitle} eq ''){
-	&printdebug("","warn","The Page has no title");
-	}else{
-	&printdebug("","pass","PageTitle: $crimp->{PageTitle}");
+   if (( -e $requested ) && ( !-d $requested )) {
+		sysopen (FILE,$requested,O_RDONLY) || &printdebug('', 'warn', 'Couldn\'t open file for reading', "file: $requested", "error: $!");
+		
+		if (<FILE>) {
+			@display_content=<FILE>;
+			close(FILE);
+			
+			my $new_content='';
+			
+			####
+			foreach $display_content(@display_content) {
+				$new_content= join '', $new_content, $display_content;
+			}
+			
+			#remove xml header if present
+			$new_content =~ s|<\?xml.*?\?>||i;
+			#remove doctype if present
+			$new_content =~ s|<!DOCTYPE.*?>||i;
+			#remove headers storing the title of the page
+			$new_content =~ s|<title>(.*?)</title>||si;
+			
+			$crimp->{PageTitle} = $1;
+			if ($crimp->{PageTitle} eq ''){
+				&printdebug('','warn','The Page has no title');
+			}else{
+				&printdebug('','pass',"PageTitle: $crimp->{PageTitle}");
+			}
+			
+			#strip from <html> down to the opening <body> tag
+			$new_content =~ s|<html.*?>.*?<body>||si;
+			#remove the closing </body> tag and any cruft after - alas, that's nothing to do with the dogshow
+			$new_content =~ s|</body>.*||si;
+	    	
+			$crimp->{DisplayHtml} = $new_content;
+			
+			####
+			$crimp->{ExitCode} = '200';
+			&printdebug('','pass',"DisplayHtml filled with content from '$requested'");
+			#$crimp->{DisplayHtml}=@display_content;
+		} else {
+			&printdebug('','warn','The file handle is inalid. This should not happen.');
+		}
+	} else {
+		$crmip->{DisplayHtml} = 'Could not get the requested content. Please check the link and try again.';
+		if (!-e $requested) {
+			&printdebug('','warn',"$requested does not exist.");
+		} else {
+			&printdebug('','warn',"$requested is a directory.");
+		}
 	}
-	
-	#strip from <html> down to the opening <body> tag
-	$new_content =~ s|<html.*?>.*?<body>||si;
-	#remove the closing </body> tag and any cruft after - alas, that's nothing to do with the dogshow
-	$new_content =~ s|</body>.*||si;
-    
-    $crimp->{DisplayHtml} = $new_content;
-    
-    ####
-  #  $crimp->{ExitCode} = '200';
-   &printdebug("","pass","DisplayHtml filled with content: $requested");
-    #$crimp->{DisplayHtml}=@display_content;
 }
 
 1;
