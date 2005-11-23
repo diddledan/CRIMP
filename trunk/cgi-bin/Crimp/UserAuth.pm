d1d4ea5df681d9b3f6fe8ccfb7142820cc8edef5
diddledan <diddledan@9e2adf73-833e-0410-8d6d-913abc08db82>
2005-11-23 15:04:58 +0000

    now we have basic authentication - password only, which is set in the crimp.ini file

---
+++ trunk/cgi-bin/Crimp/UserAuth.pm
@@ -0,0 +1,46 @@
+$ID = q$Id: UserAuth.pm,v 1.1 2005-11-23 15:04:58 diddledan Exp $;
+&printdebug('Module UserAuth','',
+				'Authors: The CRIMP Team',
+				"Version: $ID",
+				'http://crimp.sourceforge.net/',
+				);
+
+if ($Config->{$crimp->{UserConfig}}->{UserAuth} ne '') {
+	# set exit code to 403 (forbidden)
+	$crimp->{PageTitle} = 'Access Denied';
+	$crimp->{ExitCode} = '403';
+	$crimp->{skipRemainingPlugins} = 1;
+	
+	$crimp->{DisplayHtml} = '
+<div style="text-align: center;">
+	<br />
+	<h2>Error 403: Forbidden</h2>
+	<br />
+	<p>You need to supply credentials for access to this area of the site</p>
+	<br />
+	<p><form method="post">
+		Please enter the pasword for this section: <input type="password" name="password" />
+		<br /><br />
+		<input type="submit" value="Access the secured section" />
+	</form></p>
+</div>
+';
+
+	$query = new CGI;
+	my $passy = $Config->{$crimp->{UserConfig}}->{UserAuth};
+	&printdebug('','',$query->param('password'),$query->cookie("$crimp->{UserConfig}:pass"));
+	if (($query->param('password') eq $passy) || ($query->cookie("$crimp->{UserConfig}:pass") eq $passy)) {
+		#clean up after ourselves, as the user is permitted access to this section...
+		push @cookies, $query->cookie(-name => "$crimp->{UserConfig}:pass",
+												-value => $passy,
+												-path => $crimp->{UserConfig},
+												);
+		$crimp->{DisplayHtml} = '';
+		$crimp->{ExitCode} = '500';
+		$crimp->{skipRemainingPlugins} = 0;
+	}
+} else {
+	&printdebug('','warn','Auth is not configured apropriately for this location');
+}
+
+1;
