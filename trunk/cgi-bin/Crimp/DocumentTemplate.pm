61cf02eb6beee8308c2d9fda7618d73dc471b243
ind-network <ind-network@9e2adf73-833e-0410-8d6d-913abc08db82>
2005-11-25 08:13:09 +0000

    Fixed bug : no page displayed when on error. Added more template control. Tidied up.

--- trunk/cgi-bin/Crimp/DocumentTemplate.pm
+++ trunk/cgi-bin/Crimp/DocumentTemplate.pm
@@ -1,4 +1,4 @@
-$ID = q$Id: DocumentTemplate.pm,v 1.7 2005-11-19 18:22:38 ind-network Exp $;
+$ID = q$Id: DocumentTemplate.pm,v 1.8 2005-11-25 08:13:09 ind-network Exp $;
 &printdebug('Module DocumentTemplate',
 			'',
 			'Authors: The CRIMP Team',
@@ -6,35 +6,41 @@ $ID = q$Id: DocumentTemplate.pm,v 1.7 2005-11-19 18:22:38 ind-network Exp $;
 			'http://crimp.sourceforge.net/'
 			);
 
+# This should also be set within the query string '?DocumentTemplate=[none|off]'
+if (($crimp->{DocumentTemplate} eq 'none')||($crimp->{DocumentTemplate} eq 'off')){
+	&printdebug("","pass","The template has been disabled");
+}else{
+
 #only parse the template if this is an html or xhtml page
-if (($crimp->{ContentType} eq 'text/html') || ($crimp->{ContentType} eq 'text/xhtml+xml')) {
-	@HttpRequest = split(/\//,$crimp->{HttpRequest});
-	foreach $HttpRequest (@HttpRequest){
-	  #print "$crimp->{HttpRequest} :: $HttpRequest :: $crimp->{UserConfig}<br>";
-	  if ($crimp->{UserConfig} ne "/$HttpRequest"){$path = "$path/$HttpRequest";}
-	}
+	if (($crimp->{ContentType} eq 'text/html') || ($crimp->{ContentType} eq 'text/xhtml+xml')) {
+		@HttpRequest = split(/\//,$crimp->{HttpRequest});
+		foreach $HttpRequest (@HttpRequest){
+		  #print "$crimp->{HttpRequest} :: $HttpRequest :: $crimp->{UserConfig}<br>";
+		  if ($crimp->{UserConfig} ne "/$HttpRequest"){$path = "$path/$HttpRequest";}
+		}
 	
-	sysopen (FILE,$crimp->{DocumentTemplate}, O_RDONLY) or exit;
-	@template_content=<FILE>;
+		sysopen (FILE,$crimp->{DocumentTemplate}, O_RDONLY) or &printdebug("","Warn","Template $crimp->{DocumentTemplate} not found");
+		@template_content=<FILE>;
 	#$SIZE=@LINES;
-	$status = 'pass';
-	close(FILE);
-	
-	my $new_content = '';
-	foreach $template_line(@template_content) {
-	  $new_content = join '', $new_content, $template_line;
-	}
+		$status = 'pass';
+		close(FILE);
+		if (@template_content){
+			my $new_content = '';
+				foreach $template_line(@template_content) {
+		  		$new_content = join '', $new_content, $template_line;
+				}
 	
 	#printdebug ("Putting Page into Template");
-	$new_content =~ s/<!--TITLE-->/$crimp->{PageTitle} - $Config->{_}->{SiteTitle}/gi;
-	$new_content =~ s/<!--PAGE_CONTENT-->/$crimp->{DisplayHtml}/gi;
+			$new_content =~ s/<!--TITLE-->/$crimp->{PageTitle} - $Config->{_}->{SiteTitle}/gi;
+			$new_content =~ s/<!--PAGE_CONTENT-->/$crimp->{DisplayHtml}/gi;
 	
-	$crimp->{DisplayHtml} = $new_content;
-	
-	&printdebug("","$status","Started With: $crimp->{DocumentTemplate}");
-} else {
-	&printdebug('', 'pass', "Skipped module for ContentType: $crimp->{ContentType}");
+			$crimp->{DisplayHtml} = $new_content;
+		}
+		
+		&printdebug("","$status","Started With: $crimp->{DocumentTemplate}");
+	} else {
+		&printdebug('', 'pass', "Skipped module for ContentType: $crimp->{ContentType}");
+	}
 }
-
 
 1;
