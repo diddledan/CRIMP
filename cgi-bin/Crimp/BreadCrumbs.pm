20e2fe4226b3152e9549e1a995e4e4a53cbceec9
deadpan110 <deadpan110@users.noreply.github.com>
2005-11-28 21:45:05 +0000

    No longer added to the beginning (or end) of a page but inserted nicely into the html.

--- cgi-bin/Crimp/BreadCrumbs.pm
+++ cgi-bin/Crimp/BreadCrumbs.pm
@@ -1,4 +1,4 @@
-$ID = q$Id: BreadCrumbs.pm,v 1.6 2005-11-28 14:33:47 deadpan110 Exp $;
+$ID = q$Id: BreadCrumbs.pm,v 1.7 2005-11-28 21:45:05 deadpan110 Exp $;
 &printdebug('Module BreadCrumbs',
 			'',
 			'Authors: The CRIMP Team',
@@ -27,12 +27,17 @@ foreach $HttpRequest (@HttpRequest){
 if (($crimp->{BreadCrumbs} eq "top") || ($crimp->{BreadCrumbs} eq "bottom") || ($crimp->{BreadCrumbs} eq "both")){
 
 	if (($crimp->{BreadCrumbs} eq "top") || ($crimp->{BreadCrumbs} eq "both")){
-		$crimp->{DisplayHtml}="<b>Location: $BreadCrumbs</b><br/>$crimp->{DisplayHtml}";
+	$newhtml = "<b>Location: $BreadCrumbs</b><br/>";
+	$crimp->{DisplayHtml} =~ s/<body>/<body>$newhtml/i;
+		#$crimp->{DisplayHtml}="<b>Location: $BreadCrumbs</b><br/>$crimp->{DisplayHtml}";
 		&printdebug("","pass","BreadCrumbs inserted at the top of the page");
 	}
 
 	if (($crimp->{BreadCrumbs} eq "bottom") || ($crimp->{BreadCrumbs} eq "both")){
-		$crimp->{DisplayHtml}="$crimp->{DisplayHtml}<br/><b>Location: $BreadCrumbs</b>";
+		
+		$newhtml = "<br/><b>Location: $BreadCrumbs</b>";
+		$crimp->{DisplayHtml} =~ s|(</body>)|$newhtml\1|i;;
+		#$crimp->{DisplayHtml}="$crimp->{DisplayHtml}<br/><b>Location: $BreadCrumbs</b>";
 		&printdebug("","pass","BreadCrumbs inserted at the bottom of the page");
 	}
 
