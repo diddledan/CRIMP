83287d3a5c49232334583c2e2b0f3be06195082e
Daniel Llewellyn <daniel@bowlhat.net>
2005-12-04 18:24:09 +0000

    d'oh! I put 'source' instead of 'src'

--- cgi-bin/Crimp/Snow.pm
+++ cgi-bin/Crimp/Snow.pm
@@ -1,6 +1,6 @@
 #This is an example of using CRIMP's addHeaderContent function to add
 #a javascript call, which in this case displays some pretty snow
 
-my $ID = q$Id: Snow.pm,v 1.1 2005-12-04 18:11:12 diddledan Exp $;
+my $ID = q$Id: Snow.pm,v 1.2 2005-12-04 18:24:09 diddledan Exp $;
 
-&addHeaderContent('<script type="text/javascript" source="/crimp_assets/js/snow.js"></script>');
+&addHeaderContent('<script type="text/javascript" src="/crimp_assets/js/snow.js"></script>');
