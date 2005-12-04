2f0802ae27469b71c4f730bad607d4adddbf5b76
Daniel Llewellyn <daniel@bowlhat.net>
2005-12-04 18:11:15 +0000

    added a festive module (Snow) to lighten up someone's winter - sorry those in the southern hemisphere who have summer at this time of year :-/

---
+++ cgi-bin/Crimp/Snow.pm
@@ -0,0 +1,6 @@
+#This is an example of using CRIMP's addHeaderContent function to add
+#a javascript call, which in this case displays some pretty snow
+
+my $ID = q$Id: Snow.pm,v 1.1 2005-12-04 18:11:12 diddledan Exp $;
+
+&addHeaderContent('<script type="text/javascript" source="/crimp_assets/js/snow.js"></script>');
