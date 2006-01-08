d28fe0cfc2f570c97964e283829bb86f71f99b7c
Daniel Llewellyn <daniel@bowlhat.net>
2006-01-08 17:02:13 +0000

    tidied up and added listing of the topmost 5 entries in the blog file

--- cgi-bin/Crimp/FlatBlog.pm
+++ cgi-bin/Crimp/FlatBlog.pm
@@ -1,4 +1,4 @@
-$ID = q$Id: FlatBlog.pm,v 1.1 2006-01-05 21:30:54 deadpan110 Exp $;
+$ID = q$Id: FlatBlog.pm,v 1.2 2006-01-08 17:02:13 diddledan Exp $;
 &printdebug('Module FlatBlog',
 			'',
 			'Authors: The CRIMP Team',
@@ -14,7 +14,7 @@ $ID = q$Id: FlatBlog.pm,v 1.1 2006-01-05 21:30:54 deadpan110 Exp $;
 #	show the 5 appropriate entries starting from value of show
 #If $BaseContent then search and display the single entry
 
-
+use URI::Escape;
 
 
 # The blog content is stored in VarDirectory to allow another module to add entries
@@ -23,27 +23,20 @@ if (-f "$crimp->{VarDirectory}/$crimp->{FlatBlog}"){}
 my $requested = "$crimp->{VarDirectory}/$crimp->{FlatBlog}";
 
 sysopen (FILE,$requested,O_RDONLY) || &printdebug('', 'fail', 'Couldn\'t open file for reading', "file: $requested", "error: $!");
-		
-		
-			@display_content=<FILE>;
-			close(FILE);
-			
-		if (@display_content) {	
-			my $new_content='';
-			
-			####
-			
+@display_content=<FILE>;
+close(FILE);
 
-			
-			
-			foreach $display_content(@display_content) {
-				$new_content= join '', $new_content, $display_content;
-			}
-			
-			
-			$crimp->{DisplayHtml} = $new_content;
-			
-			
+if (@display_content) {
+	my $new_content = '';
+	
+	####
+	foreach $display_content(@display_content) {
+		$new_content = join '', $new_content, $display_content;
+	}
+	#hacky hack to make sure that _all_ entries are available for
+	# display without having an empty entry in the blog file.
+	$new_content = join '', $new_content, '<h1>';
+	
 #	#remove xml header if present
 #	$crimp->{DisplayHtml} =~ s|<\?xml.*?\?>||i;
 #	#remove doctype if present
@@ -57,58 +50,78 @@ sysopen (FILE,$requested,O_RDONLY) || &printdebug('', 'fail', 'Couldn\'t open fi
 #	$crimp->{DisplayHtml} =~ s|<html.*?>.*?<body>||si;
 #	#remove the closing </body> tag and any cruft after - alas, that's nothing to do with the dogshow
 #	$crimp->{DisplayHtml} =~ s|</body>.*||si;
-			
-			
-#Decide what to do
-my $BaseContent = $crimp->{HttpRequest};
-$BaseContent =~ s/^$crimp->{UserConfig}\///;
-
-my $ShowContent = 0;
-if (grep /show/, $crimp->{HttpQuery}) {
-#show entries
-}
-elsif($BaseContent ne ""){
-#show a single entry
-	$crimp->{DisplayHtml} =~ s|<h1>$BaseContent</h1>(.*?)<h1>||si; #this does not work for the last entry
-	$EntryContent = $1;
-
-$crimp->{DisplayHtml} = <<ENDEOF;
+	
+	#Decide what to do
+	my $BaseContent = $crimp->{HttpRequest};
+	$BaseContent =~ s/^$crimp->{UserConfig}\/*//;
+	
+	my $ShowContent = 0;
+	
+	my $query = new CGI;
+	if ($query->param('show')) {
+	#show entries
+	} elsif($BaseContent ne '') {
+		#show a single entry
+		$new_content =~ m|<h1>($BaseContent)</h1>(.*?)<h1>|si;
+		my $EntryTitle = $1;
+		my $EntryContent = $2;
+		# I amended the above to use the title specified
+		# in the blog file, not the one parsed from the
+		# query string. This ensures the title is displayed
+		# as the author intended. (Fremen)
+		
+		$crimp->{DisplayHtml} = <<ENDEOF;
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
   <meta
  content="text/html; charset=ISO-8859-1"
  http-equiv="content-type"/>
-  <title>$BaseContent</title>
+  <title>$EntryTitle</title>
 </head>
 <body>
-<h1><a href=\"$crimp->{HttpRequest}\">$BaseContent<a></h1>
+<h1><a href="$crimp->{HttpRequest}">$EntryTitle<a></h1>
 $EntryContent
 </body>
 </html>
 
 ENDEOF
-
-}
-
-else{
-#we have nothing to show so we will show the 1st 5 entries
-}
-
-
-&printdebug(
-    '',
-    '',
-    "BaseContent: $BaseContent",
-    "HttpQuery: $crimp->{HttpQuery}"
-);
-
-
-		}
-else {
-$crimp->{DisplayHtml} = "<span style='color: #f00;'>There are no entries in $crimp->{VarDirectory}/$crimp->{FlatBlog}</span>";
-}
-
+	} else {
+		#we have nothing to show so we will show the 1st 5 entries
+		&printdebug('','','Displaying the latest 5 entries');
+		$crimp->{DisplayHtml} = <<EOF;
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta
+ content="text/html; charset=ISO-8859-1"
+ http-equiv="content-type"/>
+  <title>Blog</title>
+</head>
+<body>
+EOF
 		
-			
+		for (my $counter = 0; $counter < 5; $counter++) {
+			if ($new_content =~ s|<h1>(.*?)</h1>(.*?)<h1>|<h1>|si) {
+				my $newurl = join '/', $crimp->{UserConfig}, uri_escape($1);
+				$newurl =~ s|/{2,}|/|g;
+				$crimp->{DisplayHtml} = join '', $crimp->{DisplayHtml}, "
+<h1><a href=\"$newurl\">$1<a></h1>
+$2
+";
+			}
+		}
+		$crimp->{DisplayHtml} = join '', $crimp->{DisplayHtml}, '</body></html>';
+	}
+	
+	&printdebug(
+		'',
+		'',
+		"BaseContent: $BaseContent",
+		"HttpQuery: $crimp->{HttpQuery}"
+	);
+} else {
+	$crimp->{DisplayHtml} = "<span style='color: #f00;'>There are no entries in $crimp->{VarDirectory}/$crimp->{FlatBlog}</span>";
+}
+
 1;
