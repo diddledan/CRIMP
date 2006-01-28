0f4d3d991122af229364944f6295d5d8d656e2d2
deadpan110 <deadpan110@users.noreply.github.com>
2006-01-28 15:49:00 +0000

    BlackRounded Style for ButtonBar

---
+++ public_html/crimp_assets/ButtonBar/BlackRounded/style.pm
@@ -0,0 +1,15 @@
+$ButtonBar = <<ENDEOF;
+
+<!--BlackRounded-->
+<table width="128" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td><img src="/crimp_assets/ButtonBar/$crimp->{ButtonBar}/pics/buttonbar-left.gif" alt="" width="12" height="25" /></td>
+    <td bgcolor="#000000"><a href="$help"><img src="/crimp_assets/ButtonBar/$crimp->{ButtonBar}/pics/help.gif" alt="-help-" width="26" height="25" border="0" /></a></td>
+    <td bgcolor="#000000"><a href="$view"><img src="/crimp_assets/ButtonBar/$crimp->{ButtonBar}/pics/view.gif" alt="-view-" width="26" height="25" border="0" /></a></td>
+    <td bgcolor="#000000"><a href="$edit"><img src="/crimp_assets/ButtonBar/$crimp->{ButtonBar}/pics/edit.gif" alt="-edit-" width="26" height="25" border="0" /></a></td>
+    <td bgcolor="#000000"><a href="$debug"><img src="/crimp_assets/ButtonBar/$crimp->{ButtonBar}/pics/debug.gif" alt="-debug-" width="26" height="25" border="0" /></a></td>
+    <td><img src="/crimp_assets/ButtonBar/$crimp->{ButtonBar}/pics/buttonbar-right.gif" width="12" height="25" /></td>
+  </tr>
+</table>
+
+ENDEOF
