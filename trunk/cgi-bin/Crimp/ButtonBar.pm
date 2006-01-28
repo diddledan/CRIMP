d16e2c72b9a120b2ee3cf6cab55bf5fcbea526a3
deadpan110 <deadpan110@9e2adf73-833e-0410-8d6d-913abc08db82>
2006-01-28 19:31:20 +0000

    Now works the way its supposed to

--- trunk/cgi-bin/Crimp/ButtonBar.pm
+++ trunk/cgi-bin/Crimp/ButtonBar.pm
@@ -1,4 +1,4 @@
-$ID = q$Id: ButtonBar.pm,v 1.1 2006-01-28 15:51:06 deadpan110 Exp $;
+$ID = q$Id: ButtonBar.pm,v 1.2 2006-01-28 19:31:20 deadpan110 Exp $;
 &printdebug('Module ButtonBar',
 			'',
 			'Authors: The CRIMP Team',
@@ -8,34 +8,47 @@ $ID = q$Id: ButtonBar.pm,v 1.1 2006-01-28 15:51:06 deadpan110 Exp $;
 
 &printdebug('','',"Started With: $crimp->{ButtonBar}");
 
-$help ="#";
-$view = "#";
-$edit = "#";
-$debug = "$crimp->{HttpRequest}?debug=on";
+my $help ="#";
+my $view = "#";
+my $edit = "#";
+my $debug = "$crimp->{HttpRequest}?debug=on";
 
-if ($crimp->{ButtonBar} eq "Default"){
 
-$ButtonBar = <<ENDEOF;
-<a href="$help"><img
+@ButtonBar = <<ENDEOF;
+<a href="<!--help-->"><img
  src="/crimp_assets/ButtonBar/$crimp->{ButtonBar}/pics/help.gif" alt="Help"
- style="border: 0px solid ; width: 26px; height: 25px;"/></a><a href="$view"><img
+ style="border: 0px solid ; width: 26px; height: 25px;"/></a><a href="<!--view-->"><img
  src="/crimp_assets/ButtonBar/$crimp->{ButtonBar}/pics/view.gif" alt="View"
- style="border: 0px solid ; width: 26px; height: 25px;"/></a><a href="$edit"><img
+ style="border: 0px solid ; width: 26px; height: 25px;"/></a><a href="<!--edit-->"><img
  src="/crimp_assets/ButtonBar/$crimp->{ButtonBar}/pics/edit.gif" alt="Edit"
- style="border: 0px solid ; width: 26px; height: 25px;"/></a><a href="$debug"><img
+ style="border: 0px solid ; width: 26px; height: 25px;"/></a><a href="<!--debug-->"><img
  src="/crimp_assets/ButtonBar/$crimp->{ButtonBar}/pics/debug.gif" alt="Debug"
  style="border: 0px solid ; width: 26px; height: 25px;"/></a>
 ENDEOF
 
-}
 
+if ($crimp->{ButtonBar} eq "Default"){
+&printdebug('','pass',"Using Default ButtonBar");
+}else{
+# Use a Custom ButtonBar
+my $requested = "$crimp->{HtmlDirectory}/crimp_assets/ButtonBar/$crimp->{ButtonBar}/style.htm";
+if ( -f $requested ) {
+		sysopen (FILE,$requested,O_RDONLY) || &printdebug('', 'fail', 'Couldn\'t open file for reading', "file: $requested", "error: $!");
+			&printdebug('','pass',"Using $crimp->{ButtonBar} ButtonBar");
+			@ButtonBar=<FILE>;
+			close(FILE);
+	}else{
+&printdebug('','warn',"$crimp->{ButtonBar} ButtonBar does not exist","Using Default ButtonBar");	
+			}
+	}
 
-else{
-#Error check here and tell the user within debug to set HtmlDirectory in crimp.ini
+# Put it all together
 
-require "$crimp->{HtmlDirectory}/crimp_assets/ButtonBar/$crimp->{ButtonBar}/style.pm";
-}
-	
-$crimp->{DisplayHtml} =~ s/<!--BUTTONBAR-->/$ButtonBar/gi;
+$crimp->{DisplayHtml} =~ s/<!--BUTTONBAR-->/@ButtonBar/gi;
+$crimp->{DisplayHtml} =~ s/<!--help-->/$help/gi;
+$crimp->{DisplayHtml} =~ s/<!--view-->/$view/gi;
+$crimp->{DisplayHtml} =~ s/<!--edit-->/$edit/gi;
+$crimp->{DisplayHtml} =~ s/<!--debug-->/$debug/gi;
+
 	
 1,
