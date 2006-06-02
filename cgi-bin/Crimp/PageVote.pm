8c2097b68380a96a673a8893606935e81b16f6b4
Daniel Llewellyn <daniel@bowlhat.net>
2006-06-02 19:08:36 +0000

    long awaited fix for PageVote to use the new CRIMP2.0 architecture

--- cgi-bin/Crimp/PageVote.pm
+++ cgi-bin/Crimp/PageVote.pm
@@ -1,282 +1,268 @@
-$ID = q$Id: PageVote.pm,v 2.0 2006-03-13 23:48:34 diddledan Exp $;
-&printdebug('Module PageVote',
+package Crimp::PageVote;
+
+sub new {
+	my ($class, $crimp) = @_;
+        my $self = {
+        	id => q$Id: PageVote.pm,v 2.1 2006-06-02 19:08:36 diddledan Exp $,
+                crimp => $crimp,
+                YesVoteRating => 0,
+		NoVoteRating => 0,
+		PageRating => 0,
+		YesVotePage => 0,
+		YesVoteSite => 0,
+		NoVotePage => 0,
+		NoVoteSite => 0,
+		StarRating => 0,
+		TotalRating => 0,
+                message => 0,
+        };
+        bless $self, $class;
+}
+
+sub execute {
+	my $self = shift;
+
+        $self->{crimp}->printdebug('Module PageVote',
 			'',
 			'Authors: The CRIMP Team',
 			"Version: $ID",
 			'http://crimp.sourceforge.net/'
 			);
-			
-&printdebug('','',"Started With: $crimp->{PageVote}");
 
-####################################################################
-my $YesVoteRating = 0;
-my $NoVoteRating = 0;
-my $PageRating = 0;
-my $YesVotePage = 0;
-my $YesVoteSite = 0;		# declare these for later use
-my $NoVotePage = 0;
-my $NoVoteSite = 0;
-my $StarRating = 0;
-my $TotalRating = 0;
+	$self->{crimp}->printdebug('','',"Started With: $self->{crimp}->{PageVote}");
 
-my $message = 0;
+	####################################################################
 
-if (param('PageVote')){
-	if ((param('PageVote') eq 'yes')
-		||(param('PageVote') eq 'good')
-		||(param('PageVote') eq 'ok')){
-				$message = 1;
-	&PageVote('Yes');
-}
-
-elsif ((param('PageVote') eq 'no')
-		||(param('PageVote') eq 'poor')
-		||(param('PageVote') eq 'bad')) {
-				$message = 2;
-	&PageVote('No');
-}
-else{
-	$message = 5;
+	if ($self->{crimp}->queryParam('PageVote')) {
+		if (($self->{crimp}->queryParam('PageVote') eq 'yes')
+			||($self->{crimp}->queryParam('PageVote') eq 'good')
+			||($self->{crimp}->queryParam('PageVote') eq 'ok')) {
+				$self->{message} = 1;
+				$self->PageVote('Yes');
+		} elsif (($self->{crimp}->queryParam('PageVote') eq 'no')
+			||($self->{crimp}->queryParam('PageVote') eq 'poor')
+			||($self->{crimp}->queryParam('PageVote') eq 'bad')) {
+				$self->{message} = 2;
+				$self->PageVote('No');
+		} else {
+			$self->{message} = 5;
+		}
 	}
-}
 
-#Get Current results
-&GetVote();
+	#Get Current results
+	$self->GetVote();
 
-####################################################################
+	####################################################################
 
-&printdebug('','',"Results (for use in comment tags within a template):",
-						"&nbsp;&nbsp;&nbsp;YesRate (%): $YesVoteRating",
-						"&nbsp;&nbsp;&nbsp;NoRate (%): $NoVoteRating",
-						"&nbsp;&nbsp;&nbsp;TotalRate (%): $PageRating",
-						"&nbsp;&nbsp;&nbsp;YesVotesTotal: $YesVotePage",
-						"&nbsp;&nbsp;&nbsp;YesSiteTotal: $YesVoteSite",
-						"&nbsp;&nbsp;&nbsp;NoVotesTotal: $NoVotePage",
-						"&nbsp;&nbsp;&nbsp;NoSiteTotal: $NoVoteSite",
-						"&nbsp;&nbsp;&nbsp;StarRating (1 to 10): $StarRating",
-						"&nbsp;&nbsp;&nbsp;TotalRating (1 to 10): $TotalRating",
+	$self->{crimp}->printdebug('','',"Results (for use in comment tags within a template):",
+						"&nbsp;&nbsp;&nbsp;YesRate (%): $self->{YesVoteRating}",
+						"&nbsp;&nbsp;&nbsp;NoRate (%): $self->{NoVoteRating}",
+						"&nbsp;&nbsp;&nbsp;TotalRate (%): $self->{PageRating}",
+						"&nbsp;&nbsp;&nbsp;YesVotesTotal: $self->{YesVotePage}",
+						"&nbsp;&nbsp;&nbsp;YesSiteTotal: $self->{YesVoteSite}",
+						"&nbsp;&nbsp;&nbsp;NoVotesTotal: $self->{NoVotePage}",
+						"&nbsp;&nbsp;&nbsp;NoSiteTotal: $self->{NoVoteSite}",
+						"&nbsp;&nbsp;&nbsp;StarRating (1 to 10): $self->{StarRating}",
+						"&nbsp;&nbsp;&nbsp;TotalRating (1 to 10): $self->{TotalRating}",
 						);
 
-
-
-my @PageVote = <<ENDEOF;
+	my @PageVote = <<ENDEOF;
 <a name="PageVote" id="PageVote"></a>
 <hr/>
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
-    <tr>
-      <td width="33%" align="left" valign="top"><font size='-1'><em><strong>Did you find this page useful?</strong></em><br />
-          
-          <form id="crimpPageVote" name="crimpPageVote" method="get" action="">
-          	<strong>Vote:</strong><input name="PageVote" type="submit" id="PageVote" value="yes" />        
-				<input name="PageVote" type="submit" value="no" />
-          </form> 
-               </td>
-      <td width="34%" align="center" valign="top"><p align="center"><!--SubmitMessage--></p>
-      </td>
-      <td width="33%" align="right" valign="top"><font size='-1'>Current Rating : <!--TotalRate-->%<br/><br/>
-       </font></td>
-    </tr>
-  </table>
+  <tr>
+    <td width="33%" align="left" valign="top"><font size='-1'><em><strong>Did you find this page useful?</strong></em><br />
+      <form id="crimpPageVote" name="crimpPageVote" method="get" action="">
+        <strong>Vote:</strong><input name="PageVote" type="submit" id="PageVote" value="yes" /><input name="PageVote" type="submit" value="no" />
+      </form>
+    </td>
+    <td width="34%" align="center" valign="top"><p align="center"><!--SubmitMessage--></p></td>
+    <td width="33%" align="right" valign="top"><font size='-1'>Current Rating : <!--TotalRate-->%<br/><br/></font></td>
+  </tr>
+</table>
 
 ENDEOF
 
-####################################################################
+	####################################################################
 
-if ($crimp->{PageVote} ne "Default"){
-# Use a Custom Template
-my $requested = "$crimp->{HtmlDirectory}/crimp_assets/PageVote/$crimp->{PageVote}/style.htm";
-if ( -f $requested ) {
-		sysopen (FILE,$requested,O_RDONLY) || &printdebug('', 'fail', 'Couldnt open file for reading', "file: $requested", "error: $!");
+	if ($self->{crimp}->{PageVote} ne 'Default') {
+		# Use a Custom Template
+		my $requested = join '', $self->{crimp}->HtmlDirectory, '/crimp_assets/PageVote/', $self->{crimp}->{PageVote}, '/style.htm';
+		if ( -f $requested ) {
+			sysopen (FILE,$requested,O_RDONLY) || $self->{crimp}->printdebug('', 'fail', 'Couldnt open file for reading', "file: $requested", "error: $!");
 			@PageVote=<FILE>;
-		close(FILE);
-	}else{	
-		&printdebug('','warn',"Template '$crimp->{PageVote}' does not exist, using 'Default'");
-		$crimp->{PageVote} ="Default";	
-			}
+			close(FILE);
+		} else {
+			$self->{crimp}->printdebug('','warn',"Template '$requested' does not exist, using 'Default'");
+			$self->{crimp}->{PageVote} = 'Default';
+		}
 	}
 
+	$PageVote = join '', @PageVote;
 
-$PageVote = join ('',@PageVote);
+	####################################################################
 
-####################################################################
+	#####
+	# All these tags need clearing before sending them into the page or menu
 
-#####
-# All these tags need clearing before sending them into the page or menu
-
-if($message eq 1){
-	if ($PageVote =~ m/<!--YesVote:/){
-		$PageVote =~ s!<\!--YesVote:(.*?)-->!!is;
-		$message = $1;
-		}else{
-			$message="Thankyou for voting yes!";
-		
-	}	
-}
-
-elsif($message eq 2){
-	if ($PageVote =~ m/<!--NoVote:/){
-		$PageVote =~ s!<\!--NoVote:(.*?)-->!!is;
-		$message = $1;
-		}else{
-			$message="Maybe you should join our mailing list and tell us how we can improve this page.";
-		
-	}	
-}
-
-elsif($message eq 3){
-	if ($PageVote =~ m/<!--VoidVote:/){
-		$PageVote =~ s!<\!--VoidVote:(.*?)-->!!is;
-		$message = $1;
-		}else{
-			$message="You have already voted on this page today!";
-		
-	}	
-}
-
-elsif($message eq 4){
-	if ($PageVote =~ m/<!--ReVote:/){
-		$PageVote =~ s!<\!--ReVote:(.*?)-->!!is;
-		$message = $1;
-		}else{
-			$message="Thankyou for coming back to vote on this page!";
-		
-	}	
-}	
-
-elsif($message eq 5){
-	if ($PageVote =~ m/<!--InvalidVote:/){
-		$PageVote =~ s!<\!--InvalidVote:(.*?)-->!!is;
-		$message = $1;
-		}else{
-			$message="Oooh... look at you!!";
-		
-	}	
-}else{$message = "&nbsp;";}
-
-$PageVote =~ s/<!--YesRate-->/$YesVoteRating/gi;
-$PageVote =~ s/<!--NoRate-->/$NoVoteRating/gi;
-$PageVote =~ s/<!--TotalRate-->/$PageRating/gi;
-
-$PageVote =~ s/<!--YesVotesTotal-->/$YesVotePage/gi;
-$PageVote =~ s/<!--NoVotesTotal-->/$NoVotePage/gi;
-
-$PageVote =~ s/<!--YesSiteTotal-->/$YesVoteSite/gi;
-$PageVote =~ s/<!--NoSiteTotal-->/$NoVoteSite/gi;
-
-$PageVote =~ s/<!--StarRating-->/$StarRating/gi;
-$PageVote =~ s/<!--TotalRating-->/$TotalRating/gi;
-$PageVote =~ s/<!--SubmitMessage-->/$message/gi;
-
-
-####################################################################
-## Decide if we are adding to the page or the menu ##
-####################################################
-
-if($PageVote =~ m/<!--MenuContent/){
-&printdebug('','pass',"Using '$crimp->{PageVote}' PageVote Template in Menu");
-&addMenuContent($PageVote);
-
-}else{
-&printdebug('','pass',"Using '$crimp->{PageVote}' PageVote Template in Content");
-$PageVote = join("\n","<div id='crimpPageVote'>",$PageVote,'</div>');
-$crimp->{DisplayHtml} =~ s|(</body>)|$PageVote\1|i;
-}
-
-########
-## end ##
-####################################################################
-
-sub PageVote{
-&printdebug('','',"Status: Checking for User");
-my $MyVote = shift;
-
-my $UserID = join '',$crimp->{RemoteHost},$crimp->{HttpRequest};
-my $CurrTime = time;
-my $UserTime = &FileRead('PageVoteUserTime',$UserID,$CurrTime);
-
-#24 hours = 86400 seconds
-my $TimeOut = 86400;
-
-
-if ($UserTime eq $CurrTime){
-	&printdebug('','',"Status: Adding Vote");
-	&CountVote($MyVote);
-}
-
-elsif ($CurrTime lt ($UserTime + $TimeOut)){
-	$message = 3;
-	&printdebug('','',"Status: Refusing Vote");
-}
-
-elsif ($CurrTime gt ($UserTime + $TimeOut -1)){
-	$message = 4;
-	&FileWrite('PageVoteUserTime',$UserID,$CurrTime);
-	&printdebug('','',"Status: Adding Another Vote");
-	&CountVote($MyVote);
-}
-
-} #end sub
-
-sub CountVote{
-my $MyVote = shift;
-my $VoteCountPage = &FileRead(join('','PageVote',$MyVote),$crimp->{HttpRequest},0);
-my $VoteCountSite = &FileRead(join('','PageVote',$MyVote),'TotalVote',0);
-
-$VoteCountPage++;
-$VoteCountSite++;
-
-my $VotePage = &FileWrite(join('','PageVote',$MyVote),$crimp->{HttpRequest},$VoteCountPage);
-my $VoteSite = &FileWrite(join('','PageVote',$MyVote),'TotalVote',$VoteCountSite);
-
-} #end sub
-
-sub GetVote{
-&printdebug('','',"Status: Getting Results");
-$YesVotePage = &FileRead("PageVoteYes",$crimp->{HttpRequest},0);
-$YesVoteSite = &FileRead("PageVoteYes",'TotalVote',0);
-
-$NoVotePage = &FileRead("PageVoteNo",$crimp->{HttpRequest},0);
-$NoVoteSite = &FileRead("PageVoteNo",'TotalVote',0);
-
-$YesVoteRating = 0;
-$NoVoteRating = 0;
-$PageRating = 0;
-
-my $TotalVoteSite = $YesVoteSite + $NoVoteSite;
-
-#BEWARE 'DIVIDE by ZERO' ERRORS if altered
-if ($TotalVoteSite ne 0){
-	if ($YesVotePage ne 0){
-		$YesVoteRating = int(($YesVotePage / $TotalVoteSite) * 100);
+	if($self->{message} == 1) {
+		if ($PageVote =~ m/<!--YesVote:/){
+			$PageVote =~ s!<\!--YesVote:(.*?)-->!!is;
+			$self->{message} = $1;
+		} else {
+			$self->{message} = "Thankyou for voting yes!";
+	        }
+	} elsif($self->{message} == 2) {
+		if ($PageVote =~ m/<!--NoVote:/) {
+			$PageVote =~ s!<\!--NoVote:(.*?)-->!!is;
+			$self->{message} = $1;
+		} else {
+			$self->{message} = "Maybe you should join our mailing list and tell us how we can improve this page.";
+		}
+	} elsif($self->{message} == 3) {
+		if ($PageVote =~ m/<!--VoidVote:/) {
+			$PageVote =~ s!<\!--VoidVote:(.*?)-->!!is;
+			$self->{message} = $1;
+		} else {
+			$self->{message}  ="You have already voted on this page today!";
+		}
+	} elsif($self->{message} == 4) {
+		if ($PageVote =~ m/<!--ReVote:/) {
+			$PageVote =~ s!<\!--ReVote:(.*?)-->!!is;
+			$self->{message} = $1;
+		} else {
+			$self->{message} = "Thankyou for coming back to vote on this page!";
+		}
+	} elsif($self->{message} == 5) {
+		if ($PageVote =~ m/<!--InvalidVote:/) {
+			$PageVote =~ s!<\!--InvalidVote:(.*?)-->!!is;
+			$self->{message} = $1;
+		} else {
+			$self->{message} = "Oooh... look at you!!";
+		}
+	} else {
+       		$self->{message} = "&nbsp;";
 	}
-	if ($NoVotePage ne 0){
-		$NoVoteRating = int(($NoVotePage  / $TotalVoteSite) * 100);
+
+	$PageVote =~ s/<!--YesRate-->/$self->{YesVoteRating}/gi;
+	$PageVote =~ s/<!--NoRate-->/$self->{NoVoteRating}/gi;
+	$PageVote =~ s/<!--TotalRate-->/$self->{PageRating}/gi;
+
+	$PageVote =~ s/<!--YesVotesTotal-->/$self->{YesVotePage}/gi;
+	$PageVote =~ s/<!--NoVotesTotal-->/$self->{NoVotePage}/gi;
+
+	$PageVote =~ s/<!--YesSiteTotal-->/$self->{YesVoteSite}/gi;
+	$PageVote =~ s/<!--NoSiteTotal-->/$self->{NoVoteSite}/gi;
+
+	$PageVote =~ s/<!--StarRating-->/$self->{StarRating}/gi;
+	$PageVote =~ s/<!--TotalRating-->/$self->{TotalRating}/gi;
+	$PageVote =~ s/<!--SubmitMessage-->/$self->{message}/gi;
+
+	####################################################################
+	## Decide if we are adding to the page or the menu ##
+	####################################################
+
+	if($PageVote =~ m/<!--MenuContent/) {
+		$self->{crimp}->printdebug('','pass',"Using '$self->{crimp}->{PageVote}' PageVote Template in Menu");
+		$self->{crimp}->addMenuContent($PageVote);
+	} else {
+		$self->{crimp}->printdebug('','pass',"Using '$self->{crimp}->{PageVote}' PageVote Template in Content");
+		$PageVote = join("\n","<div id='crimpPageVote'>",$PageVote,'</div>');
+		$self->{crimp}->addPageContent($PageVote);
 	}
-	$PageRating = int($YesVoteRating - $NoVoteRating);
-}
+} #end sub (execute)
 
-if (($YesVotePage ne 0) || ($NoVotePage ne 0)){
-		$StarRating = int(((((($YesVotePage - $NoVotePage) / ($YesVotePage + $NoVotePage)) * 100 ) + 100 ) / 2 ) / 10 );
-	}else{$StarRating = 5}
+sub PageVote {
+	my $self = shift;
 
-$TotalRating = int((($PageRating + 100)/2)/10);
+	$self->{crimp}->printdebug('','','Status: Checking for User');
+	my $MyVote = shift;
+
+	my $UserID = join '',$self->{crimp}->{RemoteHost},$self->{crimp}->{HttpRequest};
+	my $CurrTime = time;
+	my $UserTime = $self->{crimp}->FileRead('PageVoteUserTime',$UserID,$CurrTime);
+
+	#24 hours = 86400 seconds
+	my $TimeOut = 86400;
+
+	if ($UserTime == $CurrTime) {
+		$self->{crimp}->printdebug('','',"Status: Adding Vote");
+		$self->CountVote($MyVote);
+	} elsif ($CurrTime < ($UserTime + $TimeOut)) {
+		$self->{message} = 3;
+		$self->{crimp}->printdebug('','',"Status: Refusing Vote");
+	} elsif ($CurrTime > ($UserTime + $TimeOut - 1)) {
+		$self->{message} = 4;
+		$self->{crimp}->FileWrite('PageVoteUserTime',$UserID,$CurrTime);
+		$self->{crimp}->printdebug('','',"Status: Adding Another Vote");
+		$self->CountVote($MyVote);
+	}
+} #end sub (PageVote)
+
+sub CountVote {
+	my $self = shift;
+
+	my $MyVote = shift;
+
+	my $VoteCountPage = $self->{crimp}->FileRead(join('','PageVote',$MyVote),$self->{crimp}->{HttpRequest},0);
+	my $VoteCountSite = $self->{crimp}->FileRead(join('','PageVote',$MyVote),'TotalVote',0);
+
+	$VoteCountPage++;
+	$VoteCountSite++;
+
+	my $VotePage = $self->{crimp}->FileWrite(join('','PageVote',$MyVote),$self->{crimp}->{HttpRequest},$VoteCountPage);
+	my $VoteSite = $self->{crimp}->FileWrite(join('','PageVote',$MyVote),'TotalVote',$VoteCountSite);
+} #end sub (CountVote)
+
+sub GetVote {
+	my $self = shift;
+
+	$self->{crimp}->printdebug('','',"Status: Getting Results");
+	$self->{YesVotePage} = $self->{crimp}->FileRead("PageVoteYes",$self->{crimp}->{HttpRequest},0);
+	$self->{YesVoteSite} = $self->{crimp}->FileRead("PageVoteYes",'TotalVote',0);
+
+	$self->{NoVotePage} = $self->{crimp}->FileRead("PageVoteNo",$self->{crimp}->{HttpRequest},0);
+	$self->{NoVoteSite} = $self->{crimp}->FileRead("PageVoteNo",'TotalVote',0);
+
+	$self->{YesVoteRating} = 0;
+	$self->{NoVoteRating} = 0;
+	$self->{PageRating} = 0;
+
+	my $TotalVoteSite = $self->{YesVoteSite} + $self->{NoVoteSite};
+
+	#BEWARE 'DIVIDE by ZERO' ERRORS if altered
+	if ($TotalVoteSite != 0) {
+		if ($self->{YesVotePage} != 0) {
+			$self->{YesVoteRating} = int(($self->{YesVotePage} / $TotalVoteSite) * 100);
+		}
+		if ($self->{NoVotePage} != 0) {
+			$self->{NoVoteRating} = int(($self->{NoVotePage}  / $TotalVoteSite) * 100);
+		}
+		$self->{PageRating} = int($self->{YesVoteRating} - $self->{NoVoteRating});
+	}
+
+	if (($self->{YesVotePage} != 0) || ($self->{NoVotePage} != 0)) {
+		$self->{StarRating} = int(((((($self->{YesVotePage} - $self->{NoVotePage}) / ($self->{YesVotePage} + $self->{NoVotePage})) * 100 ) + 100 ) / 2 ) / 10 );
+	} else {
+        	$self->{StarRating} = 5;
+        }
+
+	$self->{TotalRating} = int((($self->{PageRating} + 100)/2)/10);
 
 
-# $YesVoteRating			percentage of yes votes from total yes votes
-# $NoVoteRating			percentage of no votes from total no votes
-# $PageRating				Actual percentage rating of page against entire site (-100 - +100)*
-# $YesVotePage				Actual count of yes votes for page
-# $YesVoteSite				Actual count of yes votes for entire site
-# $NoVotePage				Actual count of no votes for page
-# $NoVoteSite				Actual count of no votes for entire site
-# $StarRating				yes votes vs no votes (0 - 10)**
-# $TotalRating				overall site rating of page (0 - 10)**
-#
-#										*		0 means average site rating
-#										**		5 means neutral rating
-
-
-} #end sub
+	# $self->{YesVoteRating}	percentage of yes votes from total yes votes
+	# $self->{NoVoteRating}		percentage of no votes from total no votes
+	# $self->{PageRating}		Actual percentage rating of page against entire site (-100 - +100)*
+	# $self->{YesVotePage}		Actual count of yes votes for page
+	# $self->{YesVoteSite}		Actual count of yes votes for entire site
+	# $self->{NoVotePage}		Actual count of no votes for page
+	# $self->{NoVoteSite}		Actual count of no votes for entire site
+	# $self->{StarRating}		yes votes vs no votes (0 - 10)**
+	# $self->{TotalRating}		overall site rating of page (0 - 10)**
+	#
+	#    *		0 means average site rating
+	#    **		5 means neutral rating
+} #end sub (GetVote)
 
 
 1;
