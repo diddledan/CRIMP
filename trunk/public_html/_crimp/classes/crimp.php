2c84c33f627c5049132c5dc9e652786021de618a
diddledan <diddledan@9e2adf73-833e-0410-8d6d-913abc08db82>
2006-11-30 21:55:31 +0000

    updated to hopefully allow ohloh.net to pick up the licence declaration

--- trunk/public_html/_crimp/classes/crimp.php
+++ trunk/public_html/_crimp/classes/crimp.php
@@ -1,30 +1,28 @@
 <?php
-# CRIMP - Content Redirection Internet Management Program
-# Copyright (C) 2005-2006 The CRIMP Team
-# Authors:       The CRIMP Team
-# Project Leads: Martin "Deadpan110" Guppy <deadpan110@users.sourceforge.net>,
-#                Daniel "Fremen" Llewellyn <diddledan@users.sourceforge.net>
-# HomePage:      http://crimp.sf.net/
-#
-#####
-#
-# Revision info: $Id: crimp.php,v 1.1 2006-11-30 16:48:05 diddledan Exp $
-#
-##################################################################################
-# This library is free software; you can redistribute it and/or                  #
-# modify it under the terms of the GNU Lesser General Public                     #
-# License as published by the Free Software Foundation; either                   #
-# version 2.1 of the License, or (at your option) any later version.             #
-#                                                                                #
-# This library is distributed in the hope that it will be useful,                #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU              #
-# Lesser General Public License for more details.                                #
-#                                                                                #
-# You should have received a copy of the GNU Lesser General Public               #
-# License along with this library; if not, write to the Free Software            #
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA #
-##################################################################################
+/**
+ *CRIMP - Content Redirection Internet Management Program
+ *Copyright (C) 2005-2006 The CRIMP Team
+ *Authors:          The CRIMP Team
+ *Project Leads:    Martin "Deadpan110" Guppy <deadpan110@users.sourceforge.net>,
+ *                  Daniel "Fremen" Llewellyn <diddledan@users.sourceforge.net>
+ *                  HomePage:      http://crimp.sf.net/
+ *
+ *Revision info: $Id: crimp.php,v 1.2 2006-11-30 21:55:31 diddledan Exp $
+ *
+ *This library is free software; you can redistribute it and/or
+ *modify it under the terms of the GNU Lesser General Public
+ *License as published by the Free Software Foundation; either
+ *version 2.1 of the License, or (at your option) any later version.
+ *
+ *This library is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *Lesser General Public License for more details.
+ *
+ *You should have received a copy of the GNU Lesser General Public
+ *License along with this library; if not, write to the Free Software
+ *Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
 
 class Crimp {
     protected $_output;
@@ -286,7 +284,7 @@ Requested Document: {$this->_HTTPRequest}", PASS);
             $this->_output = preg_replace('|(</head>)|i',"$headers\n$1", $this->_output, 1);
             
             ## CHEAT CODES
-            $ver = '$Id: crimp.php,v 1.1 2006-11-30 16:48:05 diddledan Exp $';
+            $ver = '$Id: crimp.php,v 1.2 2006-11-30 21:55:31 diddledan Exp $';
             $this->_output = preg_replace('/<!--VERSION-->/i', $ver, $this->_output);
             ##
         }
--- trunk/public_html/_crimp/classes/plugin.php
+++ trunk/public_html/_crimp/classes/plugin.php
@@ -1,26 +1,29 @@
 <?php
-# plugin.php - extendable plugin base class designed for use with CRIMP
-# Copyright (C) 2005-2006 The CRIMP Team
-# Authors:       The CRIMP Team
-# Project Leads: Martin "Deadpan110" Guppy <deadpan110@users.sourceforge.net>,
-#                Daniel "Fremen" Llewellyn <diddledan@users.sourceforge.net>
-# HomePage:      http://crimp.sf.net/
-#
-##################################################################################
-# This library is free software; you can redistribute it and/or                  #
-# modify it under the terms of the GNU Lesser General Public                     #
-# License as published by the Free Software Foundation; either                   #
-# version 2.1 of the License, or (at your option) any later version.             #
-#                                                                                #
-# This library is distributed in the hope that it will be useful,                #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU              #
-# Lesser General Public License for more details.                                #
-#                                                                                #
-# You should have received a copy of the GNU Lesser General Public               #
-# License along with this library; if not, write to the Free Software            #
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA #
-##################################################################################
+/**
+ *plugin.php - extendable plugin base class designed for use with CRIMP
+ *CRIMP - Content Redirection Internet Management Program
+ *Copyright (C) 2005-2006 The CRIMP Team
+ *Authors:          The CRIMP Team
+ *Project Leads:    Martin "Deadpan110" Guppy <deadpan110@users.sourceforge.net>,
+ *                  Daniel "Fremen" Llewellyn <diddledan@users.sourceforge.net>
+ *                  HomePage:      http://crimp.sf.net/
+ *
+ *Revision info: $Id: plugin.php,v 1.2 2006-11-30 21:55:31 diddledan Exp $
+ *
+ *This library is free software; you can redistribute it and/or
+ *modify it under the terms of the GNU Lesser General Public
+ *License as published by the Free Software Foundation; either
+ *version 2.1 of the License, or (at your option) any later version.
+ *
+ *This library is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *Lesser General Public License for more details.
+ *
+ *You should have received a copy of the GNU Lesser General Public
+ *License along with this library; if not, write to the Free Software
+ *Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
 
 interface iPlugin {
     public function execute();
