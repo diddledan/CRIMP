6f813a0ccd1d842984faaf0fe13db66141b0006c
diddledan <diddledan@9e2adf73-833e-0410-8d6d-913abc08db82>
2006-11-30 19:57:11 +0000

    updated to hopefully allow ohloh.net to pick up the licence declaration

--- trunk/public_html/_crimp/plugins/perl_plugins/Crimp/PageVote.pm
+++ trunk/public_html/_crimp/plugins/perl_plugins/Crimp/PageVote.pm
@@ -1,9 +1,32 @@
+# CRIMP - Content Redirection Internet Management Program
+# Copyright (C) 2005-2006 The CRIMP Team
+# Authors:       The CRIMP Team
+# Project Leads: Martin "Deadpan110" Guppy <deadpan110@users.sourceforge.net>,
+#                Daniel "Fremen" Llewellyn <diddledan@users.sourceforge.net>
+# HomePage:      http://crimp.sf.net/
+#
+# Revision info: $Id: PageVote.pm,v 1.2 2006-11-30 19:57:11 diddledan Exp $
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
 package Crimp::PageVote;
 
 sub new {
 	my ($class, $crimp) = @_;
         my $self = {
-        	id => q$Id: PageVote.pm,v 1.1 2006-11-30 16:48:10 diddledan Exp $,
+        	id => q$Id: PageVote.pm,v 1.2 2006-11-30 19:57:11 diddledan Exp $,
                 crimp => $crimp,
                 YesVoteRating => 0,
 		NoVoteRating => 0,
