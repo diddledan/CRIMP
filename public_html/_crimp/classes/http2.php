4cd5ffdac3ee421a36afdf7abf5df7e11d7fed31
Daniel Llewellyn <daniel@bowlhat.net>
2006-11-30 21:55:31 +0000

    updated to hopefully allow ohloh.net to pick up the licence declaration

--- public_html/_crimp/classes/http2.php
+++ public_html/_crimp/classes/http2.php
@@ -1,26 +1,29 @@
 <?php
-# http2.php - extension to the HTTP PEAR module designed for use with crimp
-# Copyright (C) 2005-2006 The CRIMP Team
-# Authors:       The CRIMP Team
-# Project Leads: Martin "Deadpan110" Guppy <deadpan110@users.sourceforge.net>,
-#                Daniel "Fremen" Llewellyn <diddledan@users.sourceforge.net>
-# HomePage:      http://crimp.sf.net/
-#
-##################################################################################
-# This library is free software; you can redistribute it and/or                  #
-# modify it under the terms of the GNU Lesser General Public                     #
-# License as published by the Free Software Foundation; either                   #
-# version 2.1 of the License, or (at your option) any later version.             #
-#                                                                                #
-# This library is distributed in the hope that it will be useful,                #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU              #
-# Lesser General Public License for more details.                                #
-#                                                                                #
-# You should have received a copy of the GNU Lesser General Public               #
-# License along with this library; if not, write to the Free Software            #
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA #
-##################################################################################
+/**
+ *http2.php - extension to the HTTP PEAR module designed for use with crimp
+ *CRIMP - Content Redirection Internet Management Program
+ *Copyright (C) 2005-2006 The CRIMP Team
+ *Authors:          The CRIMP Team
+ *Project Leads:    Martin "Deadpan110" Guppy <deadpan110@users.sourceforge.net>,
+ *                  Daniel "Fremen" Llewellyn <diddledan@users.sourceforge.net>
+ *                  HomePage:      http://crimp.sf.net/
+ *
+ *Revision info: $Id: http2.php,v 1.2 2006-11-30 21:55:31 diddledan Exp $
+ *
+ *This library is free software; you can redistribute it and/or
+ *modify it under the terms of the GNU Lesser General Public
+ *License as published by the Free Software Foundation; either
+ *version 2.1 of the License, or (at your option) any later version.
+ *
+ *This library is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *Lesser General Public License for more details.
+ *
+ *You should have received a copy of the GNU Lesser General Public
+ *License along with this library; if not, write to the Free Software
+ *Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
 
 require_once('HTTP.php');
 class HTTP2 extends HTTP {
