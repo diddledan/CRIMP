23fc5eaed63d482c386b770a253442f7a073de8f
diddledan <diddledan@9e2adf73-833e-0410-8d6d-913abc08db82>
2006-11-30 19:41:13 +0000

    updated to hopefully allow ohloh.net to pick up the licence declaration

--- trunk/public_html/_crimp/plugins/contentDirectory.php
+++ trunk/public_html/_crimp/plugins/contentDirectory.php
@@ -1,36 +1,32 @@
 <?php
-# CRIMP - Content Redirection Internet Management Program
-# Copyright (C) 2005-2006 The CRIMP Team
-# Authors:       The CRIMP Team
-# Project Leads: Martin "Deadpan110" Guppy <deadpan110@users.sourceforge.net>,
-#                Daniel "Fremen" Llewellyn <diddledan@users.sourceforge.net>
-# HomePage:      http://crimp.sf.net/
-#
-#####
-#
-# Revision info: $Id: contentDirectory.php,v 1.1 2006-11-30 16:48:09 diddledan Exp $
-#
-##################################################################################
-# This library is free software; you can redistribute it and/or                  #
-# modify it under the terms of the GNU Lesser General Public                     #
-# License as published by the Free Software Foundation; either                   #
-# version 2.1 of the License, or (at your option) any later version.             #
-#                                                                                #
-# This library is distributed in the hope that it will be useful,                #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU              #
-# Lesser General Public License for more details.                                #
-#                                                                                #
-# You should have received a copy of the GNU Lesser General Public               #
-# License along with this library; if not, write to the Free Software            #
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA #
-##################################################################################
+/**
+ *CRIMP - Content Redirection Internet Management Program
+ *Copyright (C) 2005-2006 The CRIMP Team
+ *Authors:          The CRIMP Team
+ *Project Leads:    Martin "Deadpan110" Guppy <deadpan110@users.sourceforge.net>,
+ *                  Daniel "Fremen" Llewellyn <diddledan@users.sourceforge.net>
+ *                  HomePage:      http://crimp.sf.net/
+ *
+ *Revision info: $Id: contentDirectory.php,v 1.2 2006-11-30 19:39:43 diddledan Exp $
+ *
+ *This library is free software; you can redistribute it and/or
+ *modify it under the terms of the GNU Lesser General Public
+ *License as published by the Free Software Foundation; either
+ *version 2.1 of the License, or (at your option) any later version.
+ *
+ *This library is distributed in the hope that it will be useful,
+ *but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *Lesser General Public License for more details.
+ *
+ *You should have received a copy of the GNU Lesser General Public
+ *License along with this library; if not, write to the Free Software
+ *Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
 
 class contentDirectory extends plugin implements iPlugin {
     public function execute() {
         global $dbg, $crimp, $http;
-        
-        $path = '';
 
         $path = $this->httpRequest;
         $userConfig = $this->userConfig;
