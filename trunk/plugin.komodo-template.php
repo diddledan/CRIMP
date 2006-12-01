3d52b1898cad56d863639fb9236a17b3c8a7c936
diddledan <diddledan@9e2adf73-833e-0410-8d6d-913abc08db82>
2006-12-01 23:44:04 +0000

    modified to include a constructor that maps the crimp object and the debug object to internal variables.. put in another [[%(ask:)]] to query the developer what the default configuration key is called.. and finally, included the deferral code commented out for quick reference.

--- trunk/plugin.komodo-template.php
+++ trunk/plugin.komodo-template.php
@@ -1,13 +1,14 @@
 <?php
 /**
- *CRIMP - Content Redirection Internet Management Program
+ *[[%(ask0:plugin name)]]: a plugin for CRIMP
+ *(the Content Redirection Internet Management Program)
  *Copyright (C) 2005-2006 The CRIMP Team
  *Authors:          The CRIMP Team
  *Project Leads:    Martin "Deadpan110" Guppy <deadpan110@users.sourceforge.net>,
  *                  Daniel "Fremen" Llewellyn <diddledan@users.sourceforge.net>
  *                  HomePage:      http://crimp.sf.net/
  *
- *Revision info: $Id: plugin.komodo-template.php,v 2.2 2006-11-30 19:35:27 diddledan Exp $
+ *Revision info: $Id: plugin.komodo-template.php,v 2.3 2006-12-01 23:44:04 diddledan Exp $
  *
  *This library is free software; you can redistribute it and/or
  *modify it under the terms of the GNU Lesser General Public
@@ -24,9 +25,29 @@
  *Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 
-class [[%(ask0:plugin name)]] extends plugin implements iPlugin {
+class [[%(ask0:Plugin Name)]] implements iPlugin {
     public function execute() {
-        global $dbg, $crimp, $http;
+        $crimp = &$this->crimp;
+        $dbg = &$crimp->debug;
+        
+        $pluginName = '[[%(ask0:Plugin Name)]]';
+        
+        /**
+         *the value referenced by default configuration key, that you entered
+         *in the komodo dialog, is stored in the variable $config
+         */
+        if ( !($config = $crimp->Config('[[%(ask1:Default Configuration Key)]]', $this->scope, $pluginName)) ) {
+            $dbg->addDebug('Please define a <position /> tag in the config.xml file', WARN);
+            return;
+        }
+        
+        /**
+         *Uncomment this if construct if this plugin should defer itself
+         */
+        #if ( !$this->deferred ) {
+        #    $crimp->setDeferral($pluginName, $this->scope);
+        #    return;
+        #}
     }
 }
 
