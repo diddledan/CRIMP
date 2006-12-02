567c2763c190b9b97a74a05c9729b7c183dc2885
Daniel Llewellyn <daniel@bowlhat.net>
2006-12-02 00:14:27 +0000

    added in a lot of commentry and removed the $config and $dbg creation in favour of putting them inside the crimp class itself

--- public_html/_crimp/index.php
+++ public_html/_crimp/index.php
@@ -7,7 +7,7 @@
  *                  Daniel "Fremen" Llewellyn <diddledan@users.sourceforge.net>
  *                  HomePage:      http://crimp.sf.net/
  *
- *Revision info: $Id: index.php,v 1.2 2006-11-30 19:41:13 diddledan Exp $
+ *Revision info: $Id: index.php,v 1.3 2006-12-02 00:14:27 diddledan Exp $
  *
  *This library is free software; you can redistribute it and/or
  *modify it under the terms of the GNU Lesser General Public
@@ -24,34 +24,48 @@
  *Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 
+/**
+ *this sets the CRIMP_HOME constant to the directory containing this very file
+ */
 define('CRIMP_HOME', dirname(__FILE__));
 
-ini_set('include_path', './classes:'.ini_get('include_path'));
+/**
+ *tell php where to look for our classes
+ */
+set_include_path('./classes'.PATH_SEPARATOR.get_include_path());
 
-## debug routines
+/**
+ *debug routines
+ */
 require_once('Debug.php');
-$dbg = new Debug;
 
-## our own config interface to the pear module
-require_once('config.php');
-$config = new crimpConf;
-$conf = $config->get();
+/**
+ *our own config interface to the 'Config' pear module
+ */
+require_once('conf.php');
 
-## HTML manipulation library
-require_once('html.php');
-## http2 class which extends the HTTP pear module (requires PEAR module "HTTP")
-require_once('http2.php');
-$http = new HTTP2;
-## plugin architecture
+/**
+ *plugin architecture
+ */
 require_once('plugin.php');
-## Main class
+/**
+ *the HTTP pear module
+ */
+require_once('HTTP.php');
+/**
+ *Main class - this is where the bulk of the app resides
+ */
 require_once('crimp.php');
-$crimp = new Crimp;
+$crimp = new Crimp($dbg, $config);
 
-## do the plugin thing
+/**
+ *do the plugin thing
+ */
 $crimp->executePlugins();
 
-## send the completed page
+/**
+ *send the completed page
+ */
 $crimp->sendDocument();
 
 ?>
