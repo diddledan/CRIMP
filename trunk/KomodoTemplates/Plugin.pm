d93b5e65a65ca38d5f94104f52827f40ffe8d127
diddledan <diddledan@9e2adf73-833e-0410-8d6d-913abc08db82>
2007-05-29 23:17:31 +0000

    forced to be UTF-8 encoded

--- trunk/KomodoTemplates/Plugin.pm
+++ trunk/KomodoTemplates/Plugin.pm
@@ -1,16 +1,32 @@
+# CRIMP - Content Redirection Internet Management Program
+# Copyright (C) 2005-2007 The CRIMP Team
+# Authors:       The CRIMP Team
+# Project Leads: Martin "Deadpan110" Guppy <deadpan110@users.sourceforge.net>,
+#                Daniel "Fremen" Llewellyn <diddledan@users.sourceforge.net>
+# HomePage:      http://crimp.sf.net/
+#
+# Revision info: $Id: Plugin.pm,v 1.2 2007-05-29 23:17:31 diddledan Exp $
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This file is Licensed under the LGPL.
+
 package Crimp::Plugin::[[%(ask0:Plugin Name)]];
 use strict;
 
 sub new {
     my ($class, $crimp) = @_;
-    my $self = { id => q$Id: Plugin.pm,v 1.1 2006-12-24 14:56:31 diddledan Exp $, crimp => $crimp, };
+    my $self = { id => q$Id: Plugin.pm,v 1.2 2007-05-29 23:17:31 diddledan Exp $, crimp => $crimp, };
     bless $self, $class;
 }
 
 sub execute {
     my $self = shift;
     my $crimp = $self->{crimp};
-	
+
     $crimp->printdebug('Module \'[[%(ask0:Plugin Name)]]\'',
         '',
         'Authors: The CRIMP Team',
--- trunk/KomodoTemplates/plugin.php
+++ trunk/KomodoTemplates/plugin.php
@@ -1,28 +1,16 @@
 <?php
 /**
- *[[%(ask0:plugin name)]]: a plugin for CRIMP
- *(the Content Redirection Internet Management Program)
- *Copyright (C) 2005-2007 The CRIMP Team
- *Authors:          The CRIMP Team
- *Project Leads:    Martin "Deadpan110" Guppy <deadpan110@users.sourceforge.net>,
- *                  Daniel "Fremen" Llewellyn <diddledan@users.sourceforge.net>
- *                  HomePage:      http://crimp.sf.net/
+ * [[%(ask0:plugin name)]]: a plugin for CRIMP
+ * (the Content Redirection Internet Management Program)
+ * Copyright (C) 2005-2007 The CRIMP Team
+ * Authors:          The CRIMP Team
+ * Project Leads:    Martin "Deadpan110" Guppy <deadpan110@users.sourceforge.net>,
+ *                   Daniel "Fremen" Llewellyn <diddledan@users.sourceforge.net>
+ *                   HomePage:      http://crimp.sf.net/
  *
- *Revision info: $Id: plugin.php,v 1.3 2007-04-29 20:37:31 diddledan Exp $
+ * Revision info: $Id: plugin.php,v 1.4 2007-05-29 23:17:31 diddledan Exp $
  *
- *This library is free software; you can redistribute it and/or
- *modify it under the terms of the GNU Lesser General Public
- *License as published by the Free Software Foundation; either
- *version 2.1 of the License, or (at your option) any later version.
- *
- *This library is distributed in the hope that it will be useful,
- *but WITHOUT ANY WARRANTY; without even the implied warranty of
- *MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *Lesser General Public License for more details.
- *
- *You should have received a copy of the GNU Lesser General Public
- *License along with this library; if not, write to the Free Software
- *Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ * This file is released under the LGPL License.
  */
 
 class [[%(ask0:Plugin Name)]] implements iPlugin {
